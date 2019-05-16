446f780d009ba9b4d5d348afdea0919386e75e86
unknown <j.quesada@solardrone.es>
2019-05-16 18:08:18 +0200

    Añadido nuevo campo en planta: alturaBajaPrimero

--- src/app/models/planta.ts
+++ src/app/models/planta.ts
@@ -1,18 +1,19 @@
 export interface PlantaInterface {
-    id?: string;
-    nombre?: string;
-    contacto?: string;
-    telefono?: number;
-    longitud?: number;
-    latitud?: number;
-    tipo?: string;
-    potencia?: number;
-    empresaId?: string;
-    notas?: string;
-    filas?: number;
-    columnas?: number;
-    vertical?: boolean;
-    num_modulos?: number;
-    temp_limite?: number;
-    zoom?: number;
+  id?: string;
+  nombre?: string;
+  contacto?: string;
+  telefono?: number;
+  longitud?: number;
+  latitud?: number;
+  tipo?: string;
+  potencia?: number;
+  empresaId?: string;
+  notas?: string;
+  filas?: number;
+  columnas?: number;
+  vertical?: boolean;
+  num_modulos?: number;
+  temp_limite?: number;
+  zoom?: number;
+  alturaBajaPrimero?: boolean;
 }
