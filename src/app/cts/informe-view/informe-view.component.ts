bafa18ce9224cd13a5ab24b78d4c28c55a182e47
unknown <j.quesada@solardrone.es>
2019-05-21 08:53:26 +0200

    Arreglado firestore.rules

--- src/app/cts/informe-view/informe-view.component.ts
+++ src/app/cts/informe-view/informe-view.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { GLOBAL } from '../../services/global';
-import { PcInterface } from '../../models/pc';
-import { PcService } from 'src/app/services/pc.service';
-import { ActivatedRoute } from '@angular/router';
-import { AngularFireStorage } from '@angular/fire/storage';
-import { InformeService } from '../../services/informe.service';
-import { PlantaService } from '../../services/planta.service';
-import { InformeInterface } from '../../models/informe';
-import { PlantaInterface } from '../../models/planta';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { GLOBAL } from "../../services/global";
+import { PcInterface } from "../../models/pc";
+import { PcService } from "src/app/services/pc.service";
+import { ActivatedRoute } from "@angular/router";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { InformeService } from "../../services/informe.service";
+import { PlantaService } from "../../services/planta.service";
+import { InformeInterface } from "../../models/informe";
+import { PlantaInterface } from "../../models/planta";
+import { Observable } from "rxjs";
+import { take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-informe-view',
-  templateUrl: './informe-view.component.html',
-  styleUrls: ['./informe-view.component.css']
+  selector: "app-informe-view",
+  templateUrl: "./informe-view.component.html",
+  styleUrls: ["./informe-view.component.css"]
 })
 export class InformeViewComponent implements OnInit {
-
   public isLoaded1: boolean;
   public isLoaded2: boolean;
   public isLoaded3: boolean;
@@ -34,7 +33,9 @@ export class InformeViewComponent implements OnInit {
   public isLocalhost: boolean;
   public informeDownloadUrl: Observable<string>;
 
-  numSeveridad = new Array(GLOBAL.labels_severidad.length).fill(0).map( (_, i) => i + 1 );
+  numSeveridad = new Array(GLOBAL.labels_severidad.length)
+    .fill(0)
+    .map((_, i) => i + 1);
   public countSeveridad: number[];
 
   constructor(
@@ -44,13 +45,15 @@ export class InformeViewComponent implements OnInit {
     private pcService: PcService,
     private route: ActivatedRoute
   ) {
-    this.isLocalhost = location.hostname === 'localhost';
+    this.isLocalhost = location.hostname === "localhost";
     this.countSeveridad = new Array();
-    this.informeId = this.route.snapshot.paramMap.get('id');
-    this.informeService.getInforme(this.informeId).subscribe( informe => {
+    this.informeId = this.route.snapshot.paramMap.get("id");
+    this.informeService.getInforme(this.informeId).subscribe(informe => {
       this.informe = informe;
-      this.informeDownloadUrl = this.storage.ref(`informes/${this.informe.id}/informe.zip`).getDownloadURL();
-      this.plantaService.getPlanta(informe.plantaId).subscribe( planta => {
+      this.informeDownloadUrl = this.storage
+        .ref(`informes/${this.informe.id}/informe.zip`)
+        .getDownloadURL();
+      this.plantaService.getPlanta(informe.plantaId).subscribe(planta => {
         this.planta = planta;
         this.isLoaded1 = true;
       });
@@ -61,19 +64,19 @@ export class InformeViewComponent implements OnInit {
     this.getPcsList();
 
     this.chartOptions = {
-      legend: {display: false}
+      legend: { display: false }
     };
     this.dataSeveridad = {
       labels: GLOBAL.labels_severidad,
       datasets: [
-          {
-              label: 'Severidad',
-              backgroundColor: GLOBAL.colores_severidad,
-              hoverBackgroundColor: GLOBAL.colores_severidad,
-              data: [1, 1, 1, 1]
-          },
-        ]
-      };
+        {
+          label: "Severidad",
+          backgroundColor: GLOBAL.colores_severidad,
+          hoverBackgroundColor: GLOBAL.colores_severidad,
+          data: [1, 1, 1, 1]
+        }
+      ]
+    };
   }
 
   // receivePcs($event) {
@@ -83,103 +86,106 @@ export class InformeViewComponent implements OnInit {
 
   getPcsList() {
     let filtroSeveridad;
-    this.pcService.getPcs(this.informeId)
+    this.pcService
+      .getPcs(this.informeId)
       .pipe(take(1))
-      .subscribe(
-        response => {
-            this.allPcsConSeguidores = response.map( (pc, i, a) => {
-              //  Les añadimos los observables de los archivos....
-              // Se ha eliminado ya que tardaba mucho en cargar...
-              // pc.downloadUrlRjpg$ = this.storage.ref(`informes/${this.informeId}/rjpg/${pc.archivoPublico}`).getDownloadURL();
-              pc.downloadUrl$ = this.storage.ref(`informes/${this.informeId}/jpg/${pc.archivoPublico}`).getDownloadURL();
-// pc.downloadUrlVisual$ = this.storage.ref(`informes/${this.informeId}/jpgVisual/_mini_${pc.archivoPublico}`).getDownloadURL();
+      .subscribe(response => {
+        this.allPcsConSeguidores = response.map((pc, i, a) => {
+          //  Les añadimos los observables de los archivos....
+          // Se ha eliminado ya que tardaba mucho en cargar...
+          // pc.downloadUrlRjpg$ = this.storage.ref(`informes/${this.informeId}/rjpg/${pc.archivoPublico}`).getDownloadURL();
+          pc.downloadUrl$ = this.storage
+            .ref(`informes/${this.informeId}/jpg/${pc.archivoPublico}`)
+            .getDownloadURL();
+          // pc.downloadUrlVisual$ = this.storage.ref(`informes/${this.informeId}/jpgVisual/_mini_${pc.archivoPublico}`).getDownloadURL();
 
-              return pc;
-            });
-            this.seguidores = this.allPcsConSeguidores.filter( (pc, i, a) => {
-              return pc.tipo === 0;
-            });
-            this.allPcs = this.allPcsConSeguidores.filter( (pc, i, a) => {
-              return pc.tipo > 0;
+          return pc;
+        });
+        this.seguidores = this.allPcsConSeguidores.filter((pc, i, a) => {
+          return pc.tipo === 0;
+        });
+        this.allPcs = this.allPcsConSeguidores.filter((pc, i, a) => {
+          return pc.tipo > 0;
+        });
+        this.irradianciaMinima = this.allPcs.sort(
+          this.compareIrradiancia
+        )[0].irradiancia;
 
-            });
-            this.irradianciaMinima = this.allPcs.sort(this.compareIrradiancia)[0].irradiancia;
-
-            for (const j of this.numSeveridad) {
-              filtroSeveridad = this.allPcs.filter( pc => pc.severidad === j);
-              this.countSeveridad.push(filtroSeveridad.length);
-            }
-            this.initializeChart();
-            this.isLoaded2 = true;
-            // this.pcService.filteredPcs(this.allPcs);
-        },
-      );
+        for (const j of this.numSeveridad) {
+          filtroSeveridad = this.allPcs.filter(pc => pc.severidad === j);
+          this.countSeveridad.push(filtroSeveridad.length);
+        }
+        this.initializeChart();
+        this.isLoaded2 = true;
+        // this.pcService.filteredPcs(this.allPcs);
+      });
   }
 
-
   initializeChart() {
-    // console.log('count_sve', this.countSeveridad);
-
     this.dataSeveridad = {
       labels: GLOBAL.labels_severidad,
       datasets: [
-          {
-              label: 'Severidad',
-              backgroundColor: GLOBAL.colores_severidad,
-              hoverBackgroundColor: GLOBAL.colores_severidad,
-              data: this.countSeveridad
-          },
-        ]
-      };
+        {
+          label: "Severidad",
+          backgroundColor: GLOBAL.colores_severidad,
+          hoverBackgroundColor: GLOBAL.colores_severidad,
+          data: this.countSeveridad
+        }
+      ]
+    };
     this.isLoaded3 = true;
   }
 
   downloadInforme() {
-    this.storage.ref(`informes/${this.informe.id}/informe.zip`).getDownloadURL()
-    .subscribe( downloadUrl => {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = (event) => {
-      /* Create a new Blob object using the response
-      *  data of the onload object.
-      */
-      const blob = new Blob([xhr.response], { type: 'image/jpg' });
-      const a: any = document.createElement('a');
-      a.style = 'display: none';
-      document.body.appendChild(a);
-      const url = window.URL.createObjectURL(blob);
-      a.href = url;
-      a.download = `${this.informe.fecha} - ${this.planta.nombre}.zip`;
-      a.click();
-      window.URL.revokeObjectURL(url);
-    };
-    xhr.open('GET', downloadUrl);
-    xhr.send();
-  });
+    this.storage
+      .ref(`informes/${this.informe.id}/informe.zip`)
+      .getDownloadURL()
+      .subscribe(downloadUrl => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        xhr.onload = event => {
+          /* Create a new Blob object using the response
+           *  data of the onload object.
+           */
+          const blob = new Blob([xhr.response], { type: "image/jpg" });
+          const a: any = document.createElement("a");
+          a.style = "display: none";
+          document.body.appendChild(a);
+          const url = window.URL.createObjectURL(blob);
+          a.href = url;
+          a.download = `${this.informe.fecha} - ${this.planta.nombre}.zip`;
+          a.click();
+          window.URL.revokeObjectURL(url);
+        };
+        xhr.open("GET", downloadUrl);
+        xhr.send();
+      });
   }
 
   downloadInformeExcel() {
-    this.storage.ref(`informes/${this.informe.id}/excel.xlsx`).getDownloadURL()
-      .subscribe( downloadUrl => {
-      const xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = (event) => {
-        /* Create a new Blob object using the response
-        *  data of the onload object.
-        */
-        const blob = new Blob([xhr.response], { type: 'image/jpg' });
-        const a: any = document.createElement('a');
-        a.style = 'display: none';
-        document.body.appendChild(a);
-        const url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = `${this.informe.fecha}_${this.planta.nombre}.xlsx`;
-        a.click();
-        window.URL.revokeObjectURL(url);
-      };
-      xhr.open('GET', downloadUrl);
-      xhr.send();
-    });
+    this.storage
+      .ref(`informes/${this.informe.id}/excel.xlsx`)
+      .getDownloadURL()
+      .subscribe(downloadUrl => {
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        xhr.onload = event => {
+          /* Create a new Blob object using the response
+           *  data of the onload object.
+           */
+          const blob = new Blob([xhr.response], { type: "image/jpg" });
+          const a: any = document.createElement("a");
+          a.style = "display: none";
+          document.body.appendChild(a);
+          const url = window.URL.createObjectURL(blob);
+          a.href = url;
+          a.download = `${this.informe.fecha}_${this.planta.nombre}.xlsx`;
+          a.click();
+          window.URL.revokeObjectURL(url);
+        };
+        xhr.open("GET", downloadUrl);
+        xhr.send();
+      });
   }
 
   compareIrradiancia(a: PcInterface, b: PcInterface) {
@@ -194,13 +200,11 @@ export class InformeViewComponent implements OnInit {
 
   public calificacionMae(mae: number) {
     if (mae <= 0.1) {
-      return 'muy bueno';
+      return "muy bueno";
     } else if (mae <= 0.2) {
-      return 'correcto';
+      return "correcto";
     } else {
-      return 'mejorable';
+      return "mejorable";
     }
   }
-
 }
-
---
+++ src/app/cts/pc-overview/pc-overview.component.ts
@@ -0,0 +1,150 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { InformeInterface } from "src/app/models/informe";
+import { PlantaInterface } from "src/app/models/planta";
+import { PcInterface } from "src/app/models/pc";
+import { GLOBAL } from "src/app/services/global";
+
+@Component({
+  selector: "app-pc-overview",
+  templateUrl: "./pc-overview.component.html",
+  styleUrls: ["./pc-overview.component.css"]
+})
+export class PcOverviewComponent implements OnInit {
+  @Input() informe: InformeInterface;
+  @Input() planta: PlantaInterface;
+  @Input() allPcs: PcInterface[];
+
+  public numCategorias: Array<number>;
+  public numClases: Array<number>;
+  public perdidasPorCategoria: Array<number>;
+  public perdidasPorClase: Array<number>;
+  public dataPerdidasCategoria: any;
+  public dataCountCategoria: any;
+  public dataPerdidasTotales: any;
+  public perdidasPorCategoriaLabels: string[];
+  public chartOptions: any;
+  public perdidasTotales: number;
+  public dataSeveridad: any;
+  public countCategoria: number[];
+  public countCategoriaLabels: string[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.perdidasPorClase = Array();
+
+    this.numCategorias = Array(GLOBAL.labels_tipos.length)
+      .fill(0)
+      .map((_, i) => i + 1);
+    this.numClases = Array(GLOBAL.labels_severidad.length)
+      .fill(0)
+      .map((_, i) => i + 1);
+
+    // Pérdidas por categoria
+    let perdidasCategoria: number;
+    this.perdidasPorCategoria = Array();
+    this.perdidasPorCategoriaLabels = Array();
+    this.countCategoria = Array();
+    this.countCategoriaLabels = Array();
+
+    let filtroCategoria;
+    for (const i of this.numCategorias) {
+      filtroCategoria = this.allPcs.filter(pc => pc.tipo === i);
+
+      perdidasCategoria = filtroCategoria
+        .map(pc => {
+          let numeroModulos;
+          if (pc.modulosAfectados) {
+            numeroModulos = pc.modulosAfectados;
+          } else {
+            numeroModulos = 1;
+          }
+          return GLOBAL.pcPerdidas[i] * numeroModulos;
+        })
+        .reduce((a, b) => a + b, 0);
+
+      if (filtroCategoria.length > 0) {
+        this.countCategoria.push(filtroCategoria.length);
+        this.countCategoriaLabels.push(GLOBAL.labels_tipos[i]);
+      }
+
+      if (perdidasCategoria > 0) {
+        this.perdidasPorCategoria.push(
+          Math.round(
+            ((this.planta.moduloPotencia / 1000) * perdidasCategoria * 10) / 10
+          )
+        );
+        this.perdidasPorCategoriaLabels.push(GLOBAL.labels_tipos[i]);
+      }
+    }
+    // Pérdidas por clase
+    let perdidasClase: number;
+    let filtroClase;
+    for (const i of this.numClases) {
+      filtroClase = this.allPcs.filter(pc => pc.severidad === i);
+
+      perdidasClase = this.allPcs
+        .filter(pc => pc.severidad === i)
+        .map(pc => {
+          let numeroModulos;
+          if (pc.modulosAfectados) {
+            numeroModulos = pc.modulosAfectados;
+          } else {
+            numeroModulos = 1;
+          }
+          return GLOBAL.pcPerdidas[pc.tipo] * numeroModulos;
+        })
+        .reduce((a, b) => a + b, 0);
+
+      this.perdidasPorClase.push(Math.round(perdidasClase * 10) / 10);
+    }
+
+    this.dataPerdidasCategoria = {
+      labels: this.countCategoriaLabels,
+      datasets: [
+        {
+          label: "Pérdidas (kW)",
+          backgroundColor: "#FF6347",
+
+          data: this.perdidasPorCategoria
+        }
+      ]
+    };
+
+    this.dataCountCategoria = {
+      labels: this.countCategoriaLabels,
+      datasets: [
+        {
+          label: "Número de anomalías",
+          backgroundColor: "grey",
+
+          data: this.countCategoria
+        }
+      ]
+    };
+
+    // Pérdidas totales en kW
+    this.perdidasTotales = Math.round(
+      this.perdidasPorCategoria.reduce((a, b) => a + b, 0)
+    );
+
+    this.dataPerdidasTotales = {
+      labels: ["Pérdidas nominales (kW)", "Potencia nominal no afectada"],
+      datasets: [
+        {
+          label: "Pérdidas",
+          backgroundColor: ["#FF6347", "#3CB371"],
+          // hoverBackgroundColor: GLOBAL.colores_severidad,
+          data: [
+            this.perdidasTotales,
+            this.planta.potencia * 1000 - this.perdidasTotales
+          ]
+        }
+      ]
+    };
+
+    this.chartOptions = {
+      legend: { display: false }
+    };
+  }
+}
