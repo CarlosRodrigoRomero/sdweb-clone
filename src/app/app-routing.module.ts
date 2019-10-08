211ba9ae75fab3df6fbd4ac9dc701a2100fd01c2
unknown <j.quesada@solardrone.es>
2019-10-08 14:38:06 +0200

    routing: eliminado videos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,59 +12,59 @@ import { VideoComponent } from "./cts/video/video.component";
 import { AutoLocComponent } from "./cts/auto-loc/auto-loc.component";
 import { PlantaAddComponent } from "./cts/planta-add/planta-add.component";
 import { InformeAddComponent } from "./cts/informe-add/informe-add.component";
-import { PlantaEditComponent } from './cts/planta-edit/planta-edit.component';
+import { PlantaEditComponent } from "./cts/planta-edit/planta-edit.component";
 
 const routes: Routes = [
   {
     path: "",
     component: PubliclayoutComponent,
     children: [
-      { path: "", component: IndexComponent, pathMatch: "full" },
-      {
-        path: "manuelprodiel",
-        component: VideoComponent,
-        data: { nombre: "Manuel Antonio Romero", codigo: "5lkuxrnj7w" }
-      },
-      {
-        path: "juanforestalia",
-        component: VideoComponent,
-        data: { nombre: "Juan J. De Lama", codigo: "wwmnaco1f3" }
-      },
-      {
-        path: "juanjosevalsolar",
-        component: VideoComponent,
-        data: { nombre: "Juan José Montesinos", codigo: "p4g1rk4ew5" }
-      },
-      {
-        path: "miguelangelsolaria",
-        component: VideoComponent,
-        data: { nombre: "Miguel A. Martí", codigo: "smiu0y3lql" }
-      },
-      {
-        path: "miguelacciona",
-        component: VideoComponent,
-        data: { nombre: "Miguel de Antonio Unanua", codigo: "smiu0y3lql" }
-      },
-      {
-        path: "juanantonioenel",
-        component: VideoComponent,
-        data: { nombre: "Juan Antonio Tesón", codigo: "od2t4ok22h" }
-      },
-      {
-        path: "ivangpg",
-        component: VideoComponent,
-        data: { nombre: "Iván Villamarzo", codigo: "rzuivumzbk" }
-      },
-      {
-        path: "robertoxelio",
-        component: VideoComponent,
-        data: { nombre: "Roberto R.", codigo: "idaenwgz0x" }
-      },
-      {
-        path: "paulaopde",
-        component: VideoComponent,
-        data: { nombre: "Paula Renedo", codigo: "surb5xh9q1" }
-      }
+      { path: "", component: IndexComponent, pathMatch: "full" }
+      // {
+      //   path: "manuelprodiel",
+      //   component: VideoComponent,
+      //   data: { nombre: "Manuel Antonio Romero", codigo: "5lkuxrnj7w" }
+      // },
+      // {
+      //   path: "juanforestalia",
+      //   component: VideoComponent,
+      //   data: { nombre: "Juan J. De Lama", codigo: "wwmnaco1f3" }
+      // },
+      // {
+      //   path: "juanjosevalsolar",
+      //   component: VideoComponent,
+      //   data: { nombre: "Juan José Montesinos", codigo: "p4g1rk4ew5" }
+      // },
+      // {
+      //   path: "miguelangelsolaria",
+      //   component: VideoComponent,
+      //   data: { nombre: "Miguel A. Martí", codigo: "smiu0y3lql" }
+      // },
+      // {
+      //   path: "miguelacciona",
+      //   component: VideoComponent,
+      //   data: { nombre: "Miguel de Antonio Unanua", codigo: "smiu0y3lql" }
+      // },
+      // {
+      //   path: "juanantonioenel",
+      //   component: VideoComponent,
+      //   data: { nombre: "Juan Antonio Tesón", codigo: "od2t4ok22h" }
+      // },
+      // {
+      //   path: "ivangpg",
+      //   component: VideoComponent,
+      //   data: { nombre: "Iván Villamarzo", codigo: "rzuivumzbk" }
+      // },
+      // {
+      //   path: "robertoxelio",
+      //   component: VideoComponent,
+      //   data: { nombre: "Roberto R.", codigo: "idaenwgz0x" }
+      // },
+      // {
+      //   path: "paulaopde",
+      //   component: VideoComponent,
+      //   data: { nombre: "Paula Renedo", codigo: "surb5xh9q1" }
+      // }
     ]
   },
   {
