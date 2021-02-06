7b7bf744652a780990fc9af4c3388d4ab5b0f269
Jose <j.quesada.rod@gmail.com>
2021-02-06 17:27:52 +0100

    map-all: mostrando todas las plantas (sin color)

--- src/app/clients/components/map-all-plants/map-all-plants.component.ts
+++ src/app/clients/components/map-all-plants/map-all-plants.component.ts
@@ -1,89 +1,82 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import { OSM } from 'ol/source';
-import { Tile as TileLayer } from 'ol/layer';
-import * as Proj from 'ol/proj.js';
+import Circle from 'ol/geom/Circle';
+
 import { defaults as defaultControls } from 'ol/control.js';
 
+import Feature from 'ol/Feature';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { Fill, Stroke, Style } from 'ol/style';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { switchMap } from 'rxjs/operators';
+import { fromLonLat } from 'ol/proj';
+import { PlantaInterface } from '@core/models/planta';
 import { AuthService } from '@core/services/auth.service';
 import { PlantaService } from '@core/services/planta.service';
-
-import { PlantaInterface } from '@core/models/planta';
-
-export const DEFAULT_HEIGHT = '500px';
-export const DEFAULT_WIDTH = '500px';
-
-export const DEFAULT_LAT = 40;
-export const DEFAULT_LON = -4;
 
 @Component({
   selector: 'app-map-all-plants',
   templateUrl: './map-all-plants.component.html',
   styleUrls: ['./map-all-plants.component.css'],
 })
-export class MapAllPlantsComponent implements OnInit, AfterViewInit {
+export class MapAllPlantsComponent implements OnInit {
   plantas: PlantaInterface[];
+  defaultLng = -4;
+  defaultLat = 40;
+  defalutZoom = 6;
 
-  @Input() lat: number = DEFAULT_LAT;
-  @Input() lon: number = DEFAULT_LON;
-  @Input() zoom: number;
-  @Input() width: string | number = DEFAULT_WIDTH;
-  @Input() height: string | number = DEFAULT_HEIGHT;
+  geojsonObject: any;
 
-  target: string = 'map-' + Math.random().toString(36).substring(2);
-  map: Map;
-
-  private mapEl: HTMLElement;
+  map: any;
 
   constructor(private plantaService: PlantaService, public auth: AuthService, private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    this.auth.user$.subscribe((user) =>
-      this.plantaService.getPlantasDeEmpresa(user).subscribe((plantas) => (this.plantas = plantas))
-    );
+    this.auth.user$
+      .pipe(
+        switchMap((user) => {
+          return this.plantaService.getPlantasDeEmpresa(user);
+        })
+      )
+      .subscribe((plantas) => {
+        this.plantas = plantas;
+        this.initMap();
+        const vectorSource = new VectorSource({});
+
+        plantas.forEach((planta) => {
+          vectorSource.addFeature(new Feature(new Circle(fromLonLat([planta.longitud, planta.latitud]), 1e4)));
+        });
+        const vectorLayer = new VectorLayer({
+          source: vectorSource,
+          style: new Style({
+            stroke: new Stroke({
+              color: 'red',
+              width: 2,
+            }),
+            fill: new Fill({
+              color: 'rgba(255,0,0,0.3)',
+            }),
+          }),
+        });
+        this.map.addLayer(vectorLayer);
+      });
   }
 
-  ngAfterViewInit() {
-    this.mapEl = this.elementRef.nativeElement.querySelector('#' + this.target);
-    this.setSize();
-
+  initMap() {
     this.map = new Map({
-      target: this.target,
+      target: 'map',
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
       ],
       view: new View({
-        center: Proj.fromLonLat([this.lon, this.lat]),
-        zoom: this.zoom,
+        center: fromLonLat([this.defaultLng, this.defaultLat]),
+        zoom: this.defalutZoom,
       }),
       controls: defaultControls({ attribution: false, zoom: false }).extend([]),
     });
   }
-
-  private setSize() {
-    if (this.mapEl) {
-      const styles = this.mapEl.style;
-      styles.height = coerceCssPixelValue(this.height) || DEFAULT_HEIGHT;
-      styles.width = coerceCssPixelValue(this.width) || DEFAULT_WIDTH;
-    }
-  }
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`);
-  }
-}
-
-const cssUnitsPattern = /([A-Za-z%]+)$/;
-
-function coerceCssPixelValue(value: any): string {
-  if (value == null) {
-    return '';
-  }
-
-  return cssUnitsPattern.test(value) ? value : `${value}px`;
 }
