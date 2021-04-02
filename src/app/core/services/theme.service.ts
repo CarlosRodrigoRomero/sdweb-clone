5017dce33f1b9b1eaf05d74cc95cb0a70df20d2b
ahuesoa <ahueso.87@gmail.com>
2021-04-02 19:40:46 +0200

    Recordando estado theme seleccionado

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,21 +1,34 @@
 import { Injectable, HostBinding } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
+
 import { BehaviorSubject } from 'rxjs';
+
+import { LocalStorageService } from '@core/services/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-  private _themeSelected: string = 'light-theme';
+  private _themeSelected: string = undefined;
   public themeSelected$ = new BehaviorSubject<string>(this._themeSelected);
 
-  constructor(public overlayContainer: OverlayContainer) {}
+  constructor(public overlayContainer: OverlayContainer, private localStorageService: LocalStorageService) {
+    const themeLocal = this.localStorageService.get('theme');
+    if (themeLocal !== undefined) {
+      this.themeSelected = themeLocal;
+    } else {
+      this.themeSelected = 'ligth-theme';
+    }
+  }
 
   get themeSelected() {
     return this._themeSelected;
   }
 
   set themeSelected(value: string) {
+    // guardamos el tema seleccionado en local para que quede seteado
+    this.localStorageService.set('theme', value);
+
     this._themeSelected = value;
     this.themeSelected$.next(value);
   }
