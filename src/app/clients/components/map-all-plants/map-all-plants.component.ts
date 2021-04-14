786890f2357194c93f573d970469c2e0e59e3165
ahuesoa <ahueso.87@gmail.com>
2021-04-14 15:36:52 +0200

    Hover plantas en mapa funcionando

--- src/app/clients/components/map-all-plants/map-all-plants.component.ts
+++ src/app/clients/components/map-all-plants/map-all-plants.component.ts
@@ -29,10 +29,10 @@ export class MapAllPlantsComponent implements OnInit {
   defaultLng = -4;
   defaultLat = 40;
   defalutZoom = 6;
-
   geojsonObject: any;
-
-  map: any;
+  map: Map;
+  public plantaHover: PlantaInterface;
+  private prevFeatureHover: any;
 
   constructor(
     private plantaService: PlantaService,
@@ -60,15 +60,22 @@ export class MapAllPlantsComponent implements OnInit {
         });
 
         plantas.forEach((planta) => {
-          vectorSource.addFeature(new Feature(new Circle(fromLonLat([planta.longitud, planta.latitud]), 1e4)));
-          if (
-            planta.informes !== undefined &&
-            planta.informes[0] !== undefined &&
-            planta.informes[0].mae !== undefined
-          ) {
-            style.getStroke().setColor(this.portfolioControlService.getColorMae(planta.informes[0].mae));
-            style.getFill().setColor(this.portfolioControlService.getColorMae(planta.informes[0].mae, 0.3));
+          const feature = new Feature(new Circle(fromLonLat([planta.longitud, planta.latitud]), 1e4));
+
+          if (planta.informes !== undefined && planta.informes.length > 0) {
+            const mae = planta.informes.reduce((prev, current) => (prev.fecha > current.fecha ? prev : current)).mae;
+
+            if (mae !== undefined) {
+              feature.setProperties({
+                mae,
+              });
+
+              style.getStroke().setColor(this.portfolioControlService.getColorMae(mae));
+              style.getFill().setColor(this.portfolioControlService.getColorMae(mae, 0.3));
+            }
           }
+
+          vectorSource.addFeature(feature);
         });
         const vectorLayer = new VectorLayer({
           source: vectorSource,
@@ -92,5 +99,88 @@ export class MapAllPlantsComponent implements OnInit {
       }),
       controls: defaultControls({ attribution: false, zoom: false }).extend([]),
     });
+
+    this.addPointerOnHover();
+    this.addOnHoverAction();
+
+    // this.portfolioControlService.plantaHover$.subscribe((planta) => (this.plantaHover = planta));
+  }
+
+  private addPointerOnHover() {
+    this.map.on('pointermove', (event) => {
+      if (this.map.hasFeatureAtPixel(event.pixel)) {
+        const feature = this.map.getFeaturesAtPixel(event.pixel);
+
+        if (feature.length > 0) {
+          // cambia el puntero por el de seleccionar
+          this.map.getViewport().style.cursor = 'pointer';
+        } else {
+          // vuelve a poner el puntero normal
+          this.map.getViewport().style.cursor = 'inherit';
+        }
+      } else {
+        // vuelve a poner el puntero normal
+        this.map.getViewport().style.cursor = 'inherit';
+      }
+    });
+  }
+
+  private addOnHoverAction() {
+    let currentFeatureHover;
+    this.map.on('pointermove', (event) => {
+      if (this.map.hasFeatureAtPixel(event.pixel)) {
+        const feature = this.map.getFeaturesAtPixel(event.pixel);
+
+        if (feature.length > 0) {
+          // cuando pasamos de una anomalia a otra directamente sin pasar por vacio
+          if (this.prevFeatureHover !== undefined && this.prevFeatureHover !== feature) {
+            (this.prevFeatureHover[0] as Feature).setStyle(this.getStyleOnHover(false));
+          }
+          currentFeatureHover = feature;
+
+          (feature[0] as Feature).setStyle(this.getStyleOnHover(true));
+
+          this.prevFeatureHover = feature;
+        }
+      } else {
+        this.plantaHover = undefined;
+
+        if (currentFeatureHover !== undefined) {
+          (currentFeatureHover[0] as Feature).setStyle(this.getStyleOnHover(false));
+        }
+      }
+    });
+  }
+
+  private getStyleOnHover(hovered: boolean) {
+    if (hovered) {
+      return (feature: Feature) => {
+        if (feature !== undefined) {
+          return new Style({
+            stroke: new Stroke({
+              color: 'white',
+              width: 6,
+            }),
+            fill: new Fill({
+              color: this.portfolioControlService.getColorMae(feature.getProperties().mae, 0.3),
+            }),
+          });
+        }
+      };
+    } else {
+      return (feature: Feature) => {
+        if (feature !== undefined) {
+          return new Style({
+            stroke: new Stroke({
+              color: this.portfolioControlService.getColorMae(feature.getProperties().mae),
+              width: 2,
+            }),
+            fill: new Fill({
+              color: this.portfolioControlService.getColorMae(feature.getProperties().mae, 0.3),
+            }),
+          });
+        }
+      };
+    }
   }
 }
