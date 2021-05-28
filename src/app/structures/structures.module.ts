0bc9f35bedb9917b62c623a4c1c2222163c6e8c3
ahuesoa <ahueso.87@gmail.com>
2021-05-28 14:18:46 +0200

    Llamando cloud function auto agrupaciones OK

--- src/app/structures/structures.module.ts
+++ src/app/structures/structures.module.ts
@@ -9,9 +9,10 @@ import { StructuresComponent } from './components/structures.component';
 import { MapStructuresComponent } from './components/map-structures/map-structures.component';
 import { ModuleGroupsComponent } from './components/module-groups/module-groups.component';
 import { RawModulesComponent } from './components/raw-modules/raw-modules.component';
+import { AutoModuleGroupsComponent } from './components/auto-module-groups/auto-module-groups.component';
 
 @NgModule({
-  declarations: [StructuresComponent, MapStructuresComponent, ModuleGroupsComponent, RawModulesComponent],
+  declarations: [StructuresComponent, MapStructuresComponent, ModuleGroupsComponent, RawModulesComponent, AutoModuleGroupsComponent],
   imports: [CommonModule, StructuresRoutingModule, SharedModule, FiltersModule],
 })
 export class StructuresModule {}
