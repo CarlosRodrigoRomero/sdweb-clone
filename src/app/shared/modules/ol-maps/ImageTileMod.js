202960d68e7981fe0f1676a489f1d7b62f8a580e
ahuesoa <ahueso.87@gmail.com>
2021-06-29 17:57:34 +0200

    RainbowHc funcionando muy lento

--- src/app/shared/modules/ol-maps/ImageTileMod.js
+++ src/app/shared/modules/ol-maps/ImageTileMod.js
@@ -180,13 +180,33 @@ class ImageTileMod extends Tile {
     } else if (this.thermalLayer.codificationType === 'rainbowHc') {
       const rainbowHcPalette = GLOBAL.rainbow_hc_palette;
 
-      const pixelIndex = rainbowHcPalette.indexOf(pixel);
+      let pixelIndex;
+      rainbowHcPalette.forEach((array, index) => {
+        if (this.arraysEqual(array, pixel.slice(0, 3))) {
+          pixelIndex = index;
+        }
+      });
 
-      const temp =
-        ((this.rangeTempMax - this.rangeTempMin) * pixelIndex) / (rainbowHcPalette.length + this.rangeTempMin);
+      const temp = ((this.rangeTempMax - this.rangeTempMin) * pixelIndex) / rainbowHcPalette.length + this.rangeTempMin;
 
       return temp;
     }
+  }
+
+  arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
   }
 
   transformPixels_(image) {
