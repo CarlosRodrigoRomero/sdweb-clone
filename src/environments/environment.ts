86eb72529be49eab9445307e6b354b3e63cc13e8
ahuesoa <ahueso.87@gmail.com>
2021-07-08 11:40:38 +0200

    Trabajando de dev tb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,22 +2,22 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
-//   firebaseConfig: {
-//     apiKey: 'AIzaSyBDnFom38AIb6OL7MFP53gaT9RoHl_CAx8',
-//     authDomain: 'sdweb-dev.firebaseapp.com',
-//     databaseURL: 'https://sdweb-dev.firebaseio.com',
-//     projectId: 'sdweb-dev',
-//     storageBucket: 'sdweb-dev.appspot.com',
-//     messagingSenderId: '372895502043',
-//     appId: '1:372895502043:web:07590a39aaafcf9a7d06fe',
-//     measurementId: 'G-8993HMQKET',
-//   },
-// };
-
 export const environment = {
+  production: false,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBDnFom38AIb6OL7MFP53gaT9RoHl_CAx8',
+    authDomain: 'sdweb-dev.firebaseapp.com',
+    databaseURL: 'https://sdweb-dev.firebaseio.com',
+    projectId: 'sdweb-dev',
+    storageBucket: 'sdweb-dev.appspot.com',
+    messagingSenderId: '372895502043',
+    appId: '1:372895502043:web:07590a39aaafcf9a7d06fe',
+    measurementId: 'G-8993HMQKET',
+  },
+};
+
+/* export const environment = {
   production: true,
   MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
   firebaseConfig: {
@@ -28,7 +28,7 @@ export const environment = {
     storageBucket: 'sdweb-d33ce.appspot.com',
     messagingSenderId: '229404593483',
   },
-};
+}; */
 
 /*
  * For easier debugging in development mode, you can import the following file
