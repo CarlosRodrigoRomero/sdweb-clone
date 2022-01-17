98ad0712f8875e37af2fe084479607ac9365b534
ahuesoa <ahueso.87@gmail.com>
2022-01-17 13:49:08 +0100

    Analytics añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { NavigationEnd, Router } from '@angular/router';
 
 import { ThemeService } from '@core/services/theme.service';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -13,7 +17,15 @@ export class AppComponent implements OnInit {
 
   @HostBinding('class') componentCssClass: any;
 
-  constructor(public overlayContainer: OverlayContainer, private themeService: ThemeService) {}
+  constructor(public overlayContainer: OverlayContainer, private themeService: ThemeService, private router: Router) {
+    const navEndEvents$ = this.router.events.pipe(filter((event) => event instanceof NavigationEnd));
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-76T306HFCN', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
 
   ngOnInit() {
     this.themeService.themeSelected$.subscribe((theme) => {
