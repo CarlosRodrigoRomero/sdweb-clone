2c69e8822eef0d49538d9cf8027b954744e60800
ahuesoa <ahueso.87@gmail.com>
2022-03-16 19:21:16 +0100

    Portf: aplicando media MAE ponderada a la potencia

--- src/app/core/services/portfolio-control.service.ts
+++ src/app/core/services/portfolio-control.service.ts
@@ -171,8 +171,11 @@ export class PortfolioControlService {
               }
             });
 
-            this.maeMedio = this.average(this.maePlantas);
-            this.maeSigma = this.DAM(this.maePlantas);
+            this.maeMedio = this.weightedAverage(
+              this.maePlantas,
+              this.listaPlantas.map((planta) => planta.potencia)
+            );
+            this.maeSigma = this.DAM(this.maePlantas, this.maeMedio);
             // this.maeSigma = this.standardDeviation(this.maePlantas) / 3; // DEMO
 
             initService(true);
@@ -190,6 +193,24 @@ export class PortfolioControlService {
     return avg;
   }
 
+  private weightedAverage(arrValues, arrWeights) {
+    const result = arrValues
+      .map((value, i) => {
+        const weight = arrWeights[i];
+        const sum = value * weight;
+
+        return [sum, weight];
+      })
+      .reduce(
+        (p, c) => {
+          return [p[0] + c[0], p[1] + c[1]];
+        },
+        [0, 0]
+      );
+
+    return result[0] / result[1];
+  }
+
   private standardDeviation(values) {
     const avg = this.average(values);
 
@@ -205,9 +226,7 @@ export class PortfolioControlService {
     return stdDev;
   }
 
-  private DAM(values: number[]): number {
-    const average = this.average(values);
-
+  private DAM(values: number[], average: number): number {
     // desviacion media absoluta, para que no afectanten los extremos a la desviacion
     let sumatorioDesviaciones = 0;
     values.forEach((value) => {
