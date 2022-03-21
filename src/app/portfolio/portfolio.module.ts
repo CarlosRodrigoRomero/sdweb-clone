247f97ae3b2cece64730204b1cc01e539e256c98
ahuesoa <ahueso.87@gmail.com>
2022-03-21 14:03:24 +0100

    Portf: estado portfolio terminado

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -5,9 +5,10 @@ import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EstadoComponent } from './components/estado/estado.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, EstadoComponent],
   imports: [CommonModule, PortfolioRoutingModule, SharedModule],
 })
 export class PortfolioModule {}
