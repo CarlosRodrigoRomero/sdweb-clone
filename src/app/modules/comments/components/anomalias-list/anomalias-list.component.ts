35b60328d5ab711d6f2f049b32bc6d84d90ff2ac
ahuesoa <ahueso.87@gmail.com>
2022-08-08 11:38:30 +0200

    Coment: lista basica creada

--- src/app/modules/comments/components/anomalias-list/anomalias-list.component.ts
+++ src/app/modules/comments/components/anomalias-list/anomalias-list.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+import { FilterService } from '@data/services/filter.service';
+import { ReportControlService } from '@data/services/report-control.service';
+import { AnomaliaInfoService } from '@data/services/anomalia-info.service';
+
+import { Anomalia } from '@core/models/anomalia';
+import { Seguidor } from '@core/models/seguidor';
+
+interface RowAnomData {
+  tipo: string;
+  localizacion: string;
+}
 
 @Component({
   selector: 'app-anomalias-list',
   templateUrl: './anomalias-list.component.html',
-  styleUrls: ['./anomalias-list.component.css']
+  styleUrls: ['./anomalias-list.component.css'],
 })
 export class AnomaliasListComponent implements OnInit {
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: MatTableDataSource<RowAnomData>;
+  private anomalias: Anomalia[];
+  private anomsData: RowAnomData[];
+  displayedColumns: string[] = ['tipo', 'localizacion'];
 
-  constructor() { }
+  constructor(
+    private filterService: FilterService,
+    private reportControlService: ReportControlService,
+    private anomaliaInfoService: AnomaliaInfoService
+  ) {}
 
   ngOnInit(): void {
+    this.filterService.filteredElements$.subscribe((elems) => {
+      this.anomalias = [];
+      if (this.reportControlService.plantaFija) {
+        this.anomalias = elems as Anomalia[];
+      } else {
+        elems.forEach((seg) => this.anomalias.push(...(seg as Seguidor).anomaliasCliente));
+      }
+
+      this.anomsData = [];
+      this.anomalias.forEach((anom) => {
+        this.anomsData.push({
+          tipo: this.anomaliaInfoService.getTipoLabel(anom),
+          localizacion: this.anomaliaInfoService.getLocalizacionCompleteLabel(anom, this.reportControlService.planta),
+        });
+      });
+
+      this.dataSource = new MatTableDataSource(this.anomsData);
+      this.dataSource.sort = this.sort;
+    });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
