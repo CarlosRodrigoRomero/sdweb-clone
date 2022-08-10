bab21237dda9febc6b878be59e4835ca041bc1cb
ahuesoa <ahueso.87@gmail.com>
2022-08-10 14:47:58 +0200

    Coment: mapa mostrandose

--- src/app/modules/comments/components/map-comments/map-comments.component.ts
+++ src/app/modules/comments/components/map-comments/map-comments.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import Map from 'ol/Map';
+import { defaults as defaultControls } from 'ol/control.js';
+import { fromLonLat } from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
 import { ComentariosControlService } from '@data/services/comentarios-control.service';
+import { OlMapService } from '@data/services/ol-map.service';
+import { ReportControlService } from '@data/services/report-control.service';
+
+import { PlantaInterface } from '@core/models/planta';
 
 @Component({
   selector: 'app-map-comments',
@@ -8,9 +21,47 @@ import { ComentariosControlService } from '@data/services/comentarios-control.se
   styleUrls: ['./map-comments.component.css'],
 })
 export class MapCommentsComponent implements OnInit {
-  constructor(private comentariosControlService: ComentariosControlService) {}
+  private map: Map;
+  private planta: PlantaInterface;
 
-  ngOnInit(): void {}
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private comentariosControlService: ComentariosControlService,
+    private olMapService: OlMapService,
+    private reportControlService: ReportControlService
+  ) {}
+
+  ngOnInit(): void {
+    this.planta = this.reportControlService.planta;
+
+    this.initMap();
+  }
+
+  private initMap() {
+    const satellite = new XYZ({
+      url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      crossOrigin: '',
+    });
+    const satelliteLayer = new TileLayer({
+      source: satellite,
+    });
+
+    const layers = [satelliteLayer];
+
+    const view = new View({
+      center: fromLonLat([this.planta.longitud, this.planta.latitud]),
+      zoom: this.planta.zoom,
+      minZoom: this.planta.zoom - 2,
+      maxZoom: 24,
+    });
+
+    this.subscriptions.add(
+      this.olMapService.createMap('map', layers, view, defaultControls({ attribution: false })).subscribe((map) => {
+        this.map = map;
+      })
+    );
+  }
 
   selectVistaList() {
     this.comentariosControlService.vistaSelected = 'list';
