ee05678e1c5e0e4c76cadc0340969869d9b7478e
ahuesoa <ahueso.87@gmail.com>
2022-08-31 13:28:05 +0200

    PDF: funcionando peticion a Cloud Run

---
+++ src/app/modules/shared-plants/components/pdf/pdf.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { ReportControlService } from '@data/services/report-control.service';
+
+@Component({
+  selector: 'app-pdf',
+  templateUrl: './pdf.component.html',
+  styleUrls: ['./pdf.component.css'],
+})
+export class PdfComponent implements OnInit {
+  constructor(
+    private storage: AngularFireStorage,
+    private reportControlService: ReportControlService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {}
+
+  download() {
+    const json = this.generateJson();
+    // console.log(json);
+    this.saveJson(json);
+  }
+
+  generateJson(): any {
+    const json = { idioma: 'es' };
+    const informe = this.reportControlService.informes.find(
+      (inf) => inf.id === this.reportControlService.selectedInformeId
+    );
+    const anomalias = Object.assign({}, this.reportControlService.allAnomalias);
+
+    json['informe'] = informe;
+    json['anomalias'] = anomalias;
+    json['planta'] = this.reportControlService.planta;
+    json['apartados'] = [
+      'introduccion',
+      'criterios',
+      'normalizacion',
+      'datosVuelo',
+      'irradiancia',
+      'paramsTermicos',
+      'perdidaPR',
+      'clasificacion',
+      'planoTermico',
+      'planoVisual',
+      'resultadosClase',
+      'resultadosCatergoria',
+      'resultadosPosicion',
+    ];
+
+    return json;
+  }
+
+  saveJson(json: any) {
+    const jsonString = JSON.stringify(json);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const ref = this.storage
+      .ref('')
+      .child('informes/' + this.reportControlService.selectedInformeId + '/data.json');
+    ref.put(blob).then(() => {
+      console.log('Archivo subido');
+
+      this.downloadPdf();
+    });
+  }
+
+  downloadPdf() {
+    const url = 'https://node-pdf-rcpywurt6q-uc.a.run.app';
+
+    const params = new HttpParams().set('informeId', this.reportControlService.selectedInformeId);
+
+    this.http
+      .get(url, { responseType: 'text', params })
+      .toPromise()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
