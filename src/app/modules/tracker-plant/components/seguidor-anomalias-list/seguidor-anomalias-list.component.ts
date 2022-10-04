b6e9d5497f05719e13c9c7e02229766a9940412f
ahuesoa <antolin.h@solardrone.es>
2022-10-04 12:06:18 +0200

    Views: color tipo lista anoms seguidor aplicandose

--- src/app/modules/tracker-plant/components/seguidor-anomalias-list/seguidor-anomalias-list.component.ts
+++ src/app/modules/tracker-plant/components/seguidor-anomalias-list/seguidor-anomalias-list.component.ts
@@ -126,7 +126,8 @@ export class SeguidorAnomaliasListComponent implements OnInit, AfterViewInit, On
     const colorPerdidas = Colors.getColorPerdidas(anomalia.perdidas, 1);
     const colorCCs = Colors.getColorGradNormMax(anomalia.gradienteNormalizado, 1);
     const colorGradNormMax = Colors.getColorGradNormMax(anomalia.gradienteNormalizado, 1);
-    return [colorPerdidas, colorCCs, colorGradNormMax];
+    const colorTipo = Colors.getColorTipo(anomalia.tipo);
+    return [colorPerdidas, colorCCs, colorGradNormMax, colorTipo];
   }
 
   hoverAnomalia(row: any) {
