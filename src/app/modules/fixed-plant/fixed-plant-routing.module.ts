9eb35f24bcccc5967ee53d2cdeaf414db983ae39
ahuesoa <antolin.h@solardrone.es>
2023-03-28 13:43:18 +0200

    Layout: en proceso adaptación antiguo a nueva interfaz

--- src/app/modules/fixed-plant/fixed-plant-routing.module.ts
+++ src/app/modules/fixed-plant/fixed-plant-routing.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MapViewComponent } from './components/map-view/map-view.component';
-import { ReportContentComponent } from './components/report-content/report-content.component';
-import { LostDashboardComponent } from './components/lost-dashboard/lost-dashboard.component';
-import { MapTestComponent } from './components/map-test/map-test.component';
 
 const routes: Routes = [
   {
     path: ':id',
-    component: ReportContentComponent,
-    children: [
-      { path: '', redirectTo: 'map', pathMatch: 'full' },
-      { path: 'map', component: MapTestComponent },
-      { path: 'dashboard', component: LostDashboardComponent },
-    ],
+    component: MapViewComponent,
   },
 ];
 
