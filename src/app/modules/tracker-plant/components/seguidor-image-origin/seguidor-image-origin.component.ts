8445cc9f34ea88f974940552fb52e1daa91ceabd
ahuesoa <antolin.h@solardrone.es>
2023-03-30 16:45:43 +0200

    Template: casi terminando

--- src/app/modules/tracker-plant/components/seguidor-image-origin/seguidor-image-origin.component.ts
+++ src/app/modules/tracker-plant/components/seguidor-image-origin/seguidor-image-origin.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+import { Subscription } from 'rxjs';
 
 import { ReportControlService } from '@data/services/report-control.service';
+import { ThemeService } from '@data/services/theme.service';
 
 import { PlantaInterface } from '@core/models/planta';
 
@@ -12,13 +17,29 @@ import { PlantaInterface } from '@core/models/planta';
 export class SeguidorImageOriginComponent implements OnInit {
   private planta: PlantaInterface;
   origin = 'tL';
+  theme: string;
 
-  constructor(private reportControlService: ReportControlService) {}
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private reportControlService: ReportControlService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private themeService: ThemeService
+  ) {}
 
   ngOnInit(): void {
     this.planta = this.reportControlService.planta;
 
     this.checkOrigin();
+
+    this.loadIcons();
+
+    this.subscriptions.add(
+      this.themeService.themeSelected$.subscribe((theme) => {
+        this.theme = theme.split('-')[0];
+      })
+    );
   }
 
   private checkOrigin(): void {
@@ -33,5 +54,40 @@ export class SeguidorImageOriginComponent implements OnInit {
         this.origin = 'bL';
       }
     }
+  }
+
+  loadIcons(): void {
+    this.matIconRegistry.addSvgIcon(
+      'axis_bl_dark',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-bL-dark.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_tl_dark',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-tL-dark.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_tr_dark',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-tR-dark.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_br_dark',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-bR-dark.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_bl_light',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-bL-light.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_tl_light',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-tL-light.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_tr_light',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-tR-light.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'axis_br_light',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/axis-bR-light.svg')
+    );
   }
 }
