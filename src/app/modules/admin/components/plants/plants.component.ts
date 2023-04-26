a5dd41b7ea55f657b66642301b95e8d23289fb09
ahuesoa <antolin.h@solardrone.es>
2023-04-26 11:54:01 +0200

    Admin: mostrando nombre de empresa en vez de ID en tabla plantas

--- src/app/modules/admin/components/plants/plants.component.ts
+++ src/app/modules/admin/components/plants/plants.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-import { take } from 'rxjs/operators';
+import { map, switchMap, take, takeUntil } from 'rxjs/operators';
+import { Subject, combineLatest } from 'rxjs';
 
+import { UserService } from '@data/services/user.service';
 import { PlantaService } from '@data/services/planta.service';
 
 @Component({
@@ -12,33 +14,46 @@ import { PlantaService } from '@data/services/planta.service';
   templateUrl: './plants.component.html',
   styleUrls: ['./plants.component.css'],
 })
-export class PlantsComponent implements OnInit, AfterViewInit {
+export class PlantsComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns: string[] = ['nombre', 'id', 'tipo', 'empresa', 'potencia', 'actions'];
   dataSource = new MatTableDataSource<any>();
+  private destroy$ = new Subject<void>();
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild('search') search: ElementRef;
 
-  constructor(private plantaService: PlantaService) {}
+  constructor(private plantaService: PlantaService, private userService: UserService) {}
 
   ngOnInit(): void {
     this.plantaService
       .getAllPlantas()
-      .pipe(take(1))
-      .subscribe((plantas) => {
-        const dataPlantas: any[] = [];
-        plantas.forEach((planta) => {
-          const dataPlanta = {
-            nombre: planta.nombre,
-            id: planta.id,
-            tipo: planta.tipo,
-            empresa: planta.empresa,
-            potencia: planta.potencia,
-          };
+      .pipe(
+        take(1),
+        switchMap((plantas) => {
+          const plantasObservables = plantas.map((planta) =>
+            combineLatest([this.userService.getUser(planta.empresa).pipe(take(1))]).pipe(
+              map(([user]) => {
+                let empresa = planta.empresa;
+                if (user !== null) {
+                  empresa = user.empresaNombre;
+                }
 
-          dataPlantas.push(dataPlanta);
-        });
+                return {
+                  nombre: planta.nombre,
+                  id: planta.id,
+                  tipo: planta.tipo,
+                  empresa,
+                  potencia: planta.potencia,
+                };
+              })
+            )
+          );
 
+          return combineLatest(plantasObservables);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((dataPlantas) => {
         this.dataSource.data = dataPlantas;
       });
   }
@@ -51,5 +66,10 @@ export class PlantsComponent implements OnInit, AfterViewInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
