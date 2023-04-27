e20f253602fcb6aa0dbeef7af43fdaf5e119ddd4
ahuesoa <antolin.h@solardrone.es>
2023-04-27 17:20:10 +0200

    Recal: añadido al informe Recalcular Globals

--- src/app/shared/components/report-recalc/report-recalc.component.ts
+++ src/app/shared/components/report-recalc/report-recalc.component.ts
@@ -2,13 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { ReportControlService } from '@data/services/report-control.service';
 import { AnomaliaService } from '@data/services/anomalia.service';
 import { PcService } from '@data/services/pc.service';
+import { PlantaService } from '@data/services/planta.service';
+import { OlMapService } from '@data/services/ol-map.service';
+import { ZonesService } from '@data/services/zones.service';
 
 import { InformeInterface } from '@core/models/informe';
 import { Seguidor } from '@core/models/seguidor';
+import { Anomalia } from '@core/models/anomalia';
 
 @Component({
   selector: 'app-report-recalc',
@@ -24,7 +29,10 @@ export class ReportRecalcComponent implements OnInit, OnDestroy {
     private reportControlService: ReportControlService,
     private http: HttpClient,
     private anomaliaService: AnomaliaService,
-    private pcService: PcService
+    private pcService: PcService,
+    private plantaService: PlantaService,
+    private olMapService: OlMapService,
+    private zonesService: ZonesService
   ) {}
 
   ngOnInit(): void {
@@ -63,7 +71,7 @@ export class ReportRecalcComponent implements OnInit, OnDestroy {
 
         if (object) {
           if (this.reportControlService.plantaFija) {
-            this.anomaliaService.updateAnomaliaField(anom.id, 'tipoNextYear', anom.tipo);
+            // this.anomaliaService.updateAnomaliaField(anom.id, 'tipoNextYear', anom.tipo);
           } else {
             // this.pcService.updatePcField(anom.id, 'tipoNextYear', object.tipo2);
           }
@@ -71,6 +79,31 @@ export class ReportRecalcComponent implements OnInit, OnDestroy {
         }
       });
     });
+  }
+
+  recalGlobalCoords() {
+    // nos traemos de nuevo las locAreas por si hay nuevas o se han modificado en localizaciones
+    this.plantaService
+      .getLocationsArea(this.reportControlService.plantaId)
+      .pipe(take(1))
+      .subscribe((locAreas) => {
+        const zonas = this.zonesService.getZones(this.reportControlService.planta, locAreas);
+
+        this.reportControlService.allAnomalias
+          .filter((anom) => anom.informeId === this.reportControlService.selectedInformeId)
+          .forEach((anom) => {
+            const anomCentroid = this.olMapService.getCentroid((anom as Anomalia).featureCoords);
+            const newGlobalCoords = this.plantaService.getGlobalCoordsFromLocationAreaOl(anomCentroid, zonas);
+
+            if (this.reportControlService.plantaFija) {
+              if (anom.globalCoords.toString() !== newGlobalCoords.toString()) {
+                this.anomaliaService.updateAnomaliaField(anom.id, 'globalCoords', newGlobalCoords);
+              }
+            } else {
+              this.pcService.updatePcField(anom.id, 'globalCoords', newGlobalCoords);
+            }
+          });
+      });
   }
 
   ngOnDestroy(): void {
