a4b923c1927c5b63046b249f5cc0da49da3abd2f
ahuesoa <antolin.h@solardrone.es>
2023-05-22 16:22:31 +0200

    Recalc-portf: recalcular Maes creado, utilizado y ocultado

--- src/app/shared/components/portfolio-recalc/portfolio-recalc.component.ts
+++ src/app/shared/components/portfolio-recalc/portfolio-recalc.component.ts
@@ -6,10 +6,15 @@ import { InformeService } from '@data/services/informe.service';
 import { AnomaliaService } from '@data/services/anomalia.service';
 import { ReportControlService } from '@data/services/report-control.service';
 import { PlantaService } from '@data/services/planta.service';
+import { SeguidorService } from '@data/services/seguidor.service';
 
 import { PlantaInterface } from '@core/models/planta';
 
 import { GLOBAL } from '@data/constants/global';
+import { Seguidor } from '@core/models/seguidor';
+import { Anomalia } from '@core/models/anomalia';
+import { ZonesService } from '@data/services/zones.service';
+import { InformeInterface } from '@core/models/informe';
 
 @Component({
   selector: 'app-portfolio-recalc',
@@ -23,7 +28,9 @@ export class PortfolioRecalcComponent implements OnInit {
     private informeService: InformeService,
     private anomaliaService: AnomaliaService,
     private reportControlService: ReportControlService,
-    private plantaService: PlantaService
+    private plantaService: PlantaService,
+    private seguidorService: SeguidorService,
+    private zonesService: ZonesService
   ) {}
 
   ngOnInit(): void {}
@@ -41,38 +48,63 @@ export class PortfolioRecalcComponent implements OnInit {
         take(1)
       )
       .subscribe(async (informes) => {
-        for (let index = 0; index < informes.length; index++) {
-          if (index < 10) {
-            let informe = informes[index];
-            const planta = this.plantas.find((pl) => pl.id === informe.plantaId);
-            this.anomaliaService.planta = planta;
-            if (planta.tipo === 'seguidores') {
-              await this.anomaliaService
-                .getAnomalias$(informe.id, 'pcs')
-                .pipe(take(1))
-                .toPromise()
-                .then((anomalias) => {
-                  const fixableAnoms = anomalias.filter((anom) => GLOBAL.fixableTypes.includes(anom.tipo));
-                  console.log('seguidores');
-                  console.log(informe.mae);
-                  console.log(this.reportControlService.getMae(fixableAnoms, informe.numeroModulos));
-                  // this.reportControlService.setMae(fixableAnoms, informe, 'fixablePower');
-                });
-            } else {
-              await this.anomaliaService
-                .getAnomalias$(informe.id, 'anomalias')
-                .pipe(take(1))
-                .toPromise()
-                .then((anomalias) => {
-                  const fixableAnoms = anomalias.filter((anom) => GLOBAL.fixableTypes.includes(anom.tipo));
-                  console.log('fija');
-                  console.log(informe.mae);
-                  console.log(this.reportControlService.getMae(fixableAnoms, informe.numeroModulos));
-                  // this.reportControlService.setMae(fixableAnoms, informe, 'fixablePower');
-                });
-            }
-          }
+        for (let index = 0; index < this.plantas.length; index++) {
+          const planta = this.plantas[index];
+          this.anomaliaService.planta = planta;
+
+          await this.plantaService
+            .getLocationsArea(planta.id)
+            .pipe(take(1))
+            .toPromise()
+            .then(async (locAreas) => {
+              this.zonesService.locAreas = locAreas;
+              this.zonesService.zones = this.zonesService.getZones(planta, locAreas);
+
+              const informesPlanta = informes.filter((inf) => inf.plantaId === this.plantas[index].id);
+              for (let i = 0; i < informesPlanta.length; i++) {
+                let informe = informesPlanta[i];
+
+                if (planta.tipo !== 'seguidores') {
+                  // obtenemos todas las anomalías
+                  await this.anomaliaService
+                    .getAnomaliasPlanta$(planta, [informe])
+                    .pipe(take(1))
+                    .toPromise()
+                    .then((anomalias) => {
+                      const fixableAnoms = anomalias.filter((anom) => GLOBAL.fixableTypes.includes(anom.tipo));
+
+                      this.reportControlService.setMae(fixableAnoms, informe, 'fixablePower');
+                    });
+                } else {
+                  // obtenemos todos los seguidores
+                  await this.seguidorService
+                    .getSeguidoresPlanta$(planta, [informe])
+                    .pipe(take(1))
+                    .toPromise()
+                    .then((segs) => {
+                      const seguidores = segs as Seguidor[];
+
+                      informe = this.setNumberOfModules(informe, seguidores.length, planta);
+
+                      const anomalias: Anomalia[] = [];
+                      seguidores.forEach((seg) => {
+                        if (seg.anomaliasCliente.length > 0) {
+                          anomalias.push(...seg.anomaliasCliente);
+                        }
+                      });
+                      const fixableAnoms = anomalias.filter((anom) => GLOBAL.fixableTypes.includes(anom.tipo));
+
+                      this.reportControlService.setMae(fixableAnoms, informe, 'fixablePower');
+                    });
+                }
+              }
+            });
         }
       });
+  }
+
+  private setNumberOfModules(informe: InformeInterface, numSegs: number, planta: PlantaInterface) {
+    informe.numeroModulos = numSegs * planta.filas * planta.columnas;
+    return informe;
   }
 }
