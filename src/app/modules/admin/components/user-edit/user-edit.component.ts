e35255a9ba2155a7375a0b9a16a4bedb32f22f4b
Ruben <ruben.ferruses@solardrone.es>
2023-07-24 12:19:40 +0200

    Cambios en user-edit: -Añadido el role 6 (interno) al editar el usuario -Ahora se busca el id de la empresa que esté escrita en el campo empresa y se asigna al usuario

--- src/app/modules/admin/components/user-edit/user-edit.component.ts
+++ src/app/modules/admin/components/user-edit/user-edit.component.ts
@@ -8,6 +8,9 @@ import { Subscription } from 'rxjs';
 import { UserService } from '@data/services/user.service';
 
 import { UserInterface } from '@core/models/user';
+import { EmpresaService } from '@data/services/empresa.service';
+import { take } from 'rxjs/operators';
+import { Empresa } from '@core/models/empresa';
 
 @Component({
   selector: 'app-user-edit',
@@ -20,14 +23,17 @@ export class UserEditComponent implements OnInit, OnDestroy {
   id: string;
   user: UserInterface = {};
   selectedRole: number;
-
+  empresas: Empresa[];
+  empresaSelected: Empresa;
+  statusMessage: string;
   private subscriptions: Subscription = new Subscription();
 
   constructor(
     private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private empresaService: EmpresaService
   ) {
     this.buildForm();
   }
@@ -51,6 +57,14 @@ export class UserEditComponent implements OnInit, OnDestroy {
         });
       })
     );
+
+    this.empresaService
+      .getEmpresas()
+      .pipe(take(1))
+      .subscribe((empresas) => {
+        this.empresas = empresas;
+        this.empresaSelected = this.empresas.find(empresa => empresa.nombre === this.user.empresaNombre);
+      });
   }
 
   private buildForm() {
@@ -67,6 +81,8 @@ export class UserEditComponent implements OnInit, OnDestroy {
       this.user.email = this.form.get('email').value;
       this.user.empresaNombre = this.form.get('empresa').value;
       this.user.role = Number(this.form.get('role').value);
+      this.empresaSelected = this.empresas.find(empresa => empresa.nombre === this.user.empresaNombre);
+      this.user.empresaId = this.empresaSelected.id;
 
       // Actualiza el usuario en la DB
       this.updateUser(this.user);
