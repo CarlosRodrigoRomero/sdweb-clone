0eb515fba9f72f6d168c0647f405b6d170cbac65
Ruben <ruben.ferruses@solardrone.es>
2023-07-27 17:20:40 +0200

    Cambiada la visualización de las imágenes: -Ahora se muestran menos campos (coordenadas y nombre) -Se muestra miniatura que se saca con Exifr (la librería de los metadatos) -Las imágenes se separan según sus coordenadas en grupos. Si la imagen que se está procesando se encuentra a más de 500 metros de alguna de las imágenes anteriores, se crea un nuevo grupo. -Se clasifica en tantos grupos como haya y se crean desplegables donde se pueden visualizar las imágenes del grupo.

--- src/app/modules/admin/components/flight-upload/flight-upload.component.ts
+++ src/app/modules/admin/components/flight-upload/flight-upload.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import * as exifr from 'exifr';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 export interface ImageData {
   key: string;
   value: any;
+  GPSLatitude: number;
+  GPSLongitude: number;
+  name: string;
+
+  //Pruebas para clasificar en grupos
+  lat?: number;
+  long?: number;
+  group?: number;
 }
 
 @Component({
@@ -15,10 +24,17 @@ export class FlightUploadComponent implements OnInit {
   ngOnInit(): void {
   }
   imagesData: any[] = [];
+  imageGroups: ImageData[][] = [];
+
   currentIndex = 0;
   fileName: string = "";
 
+  imageThumbnails: SafeUrl[] = [];
 
+  //Pruebas para clasificar en grupos
+  currentGroup = 0;
+
+  constructor(private sanitizer: DomSanitizer) { }
   async handleChange(event) {
     for (let i = 0; i < event.target.files.length; i++) {
       const file = event.target.files[i];
@@ -29,9 +45,61 @@ export class FlightUploadComponent implements OnInit {
         continue;
       }
 
-      const output = await exifr.parse(file);
+      var output: ImageData = await exifr.parse(file, ['GPSLatitude', 'GPSLongitude']);
+      output.name = file.name;
+
+      // Verificar si las coordenadas GPS existen
+      if (!output.GPSLatitude || !output.GPSLongitude) {
+        alert(`El archivo ${file.name} no contiene información de GPS. Este archivo se ignorará.`);
+        continue;
+      }
+
+      // Convertir las coordenadas de DMS a decimal
+      output.GPSLatitude = this.dmsToDecimal(output.GPSLatitude[0], output.GPSLatitude[1], output.GPSLatitude[2]);
+      output.GPSLongitude = this.dmsToDecimal(output.GPSLongitude[0], output.GPSLongitude[1], output.GPSLongitude[2]);
+
+      const thumbnail = await exifr.thumbnail(file);
       this.imagesData.push(output);
+      this.imageThumbnails.push(this.getSanitizedUrl(thumbnail));
+
+      // Asignar cada imagen a un grupo basado en su proximidad a todas las imágenes en el grupo
+      let assigned = false;
+      for (let group of this.imageGroups) {
+        for(let groupImage of group){
+          if (this.calculateDistance(output.GPSLatitude, output.GPSLongitude, groupImage.GPSLatitude, groupImage.GPSLongitude) <= 0.5) {
+            group.push(output);
+            assigned = true;
+            break;
+          }
+        }
+        if(assigned) break;
+      }
+      if (!assigned) {
+        this.imageGroups.push([output]);
+      }
     }
+  }
+
+  dmsToDecimal(degrees: number, minutes: number, seconds: number) {
+    return degrees + minutes / 60 + seconds / 3600;
+  }
+
+  calculateDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const R = 6371; // radio medio de la Tierra en km
+    const dLat = this.deg2rad(lat2 - lat1);
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // distancia en km
+    return d;
+  }
+
+  deg2rad(deg: number) {
+    return deg * (Math.PI / 180);
   }
 
   nextImage() {
@@ -48,5 +116,14 @@ export class FlightUploadComponent implements OnInit {
 
   get currentImage() {
     return this.imagesData[this.currentIndex];
+  }
+
+  getSanitizedUrl(buffer: Buffer | Uint8Array): SafeUrl {
+    let TYPED_ARRAY = new Uint8Array(buffer);
+    const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+      return data + String.fromCharCode(byte);
+    }, '');
+    let base64String = btoa(STRING_CHAR);
+    return this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + base64String);
   }
 }
