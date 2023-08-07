0cc6edc252d186b852bc17971241a152d4cb184f
Ruben <ruben.ferruses@solardrone.es>
2023-08-07 09:07:03 +0200

    Implentada subida de archivos -Cálculo de velocidad de subida no funciona bien todavía

--- src/app/modules/admin/components/flight-upload/flight-upload.component.ts
+++ src/app/modules/admin/components/flight-upload/flight-upload.component.ts
@@ -6,7 +6,7 @@ import TileLayer from 'ol/layer/Tile';
 import { View } from 'ol';
 import { fromLonLat, transform } from 'ol/proj';
 import { defaults as defaultControls } from 'ol/control.js';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { OlMapService } from '@data/services/ol-map.service';
 import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
@@ -15,6 +15,10 @@ import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import { Fill, Icon, Stroke, Style } from 'ol/style';
 import Polygon from 'ol/geom/Polygon';
+
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+
 
 export interface ImageData {
   key: string;
@@ -27,6 +31,8 @@ export interface ImageData {
   lat?: number;
   long?: number;
   group?: number;
+  center?: [number, number]; // Agregar esta línea para almacenar el centro del grupo
+
 }
 
 @Component({
@@ -41,6 +47,19 @@ export class FlightUploadComponent implements OnInit {
   defaultZoom = 5.5;
   map: Map;
 
+
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  task: AngularFireUploadTask;
+
+  selectedFiles: File[] = [];
+
+  uploadSpeed: number = 0; // Aquí guardaremos la velocidad de subida
+  uploadStartTime: number; // Hora de inicio de la subida
+  uploadEndTime: number; // Hora de finalización de la subida
+  totalBytesTransferred: number = 0; // Total de bytes transferidos
+
+  totalBytes: number;
   private subscriptions: Subscription = new Subscription();
 
 
@@ -83,36 +102,43 @@ export class FlightUploadComponent implements OnInit {
   currentGroup = 0;
 
   constructor(private sanitizer: DomSanitizer,
-    private olMapService: OlMapService
+    private olMapService: OlMapService,
+    private storage: AngularFireStorage,
+
   ) { }
   async handleChange(event) {
+
+    this.selectedFiles = Array.from(event.target.files);
+
+
+
     for (let i = 0; i < event.target.files.length; i++) {
       const file = event.target.files[i];
-  
+
       // Ignorar los archivos que no sean jpg dando un aviso al usuario
       if (file.type !== 'image/jpeg') {
         alert(`Estás intentando subir el archivo ${file.name} y es de tipo ${file.type}. Este archivo se ignorará. Solo se procesarán archivos .jpg`);
         continue;
       }
-  
+
       const output: ImageData = await exifr.parse(file, ['GPSLatitude', 'GPSLongitude']);
-  
+
       // Comprobar si el archivo tiene información de coordenadas GPS
       if (!output || !output.GPSLatitude || !output.GPSLongitude) {
         alert(`El archivo ${file.name} no contiene información de GPS. Este archivo se ignorará.`);
       }
-      else{
-  
+      else {
+
         output.name = file.name;
-  
+
         // Convertir las coordenadas de DMS a decimal
         output.GPSLatitude = this.dmsToDecimal(output.GPSLatitude[0], output.GPSLatitude[1], output.GPSLatitude[2]);
         output.GPSLongitude = this.dmsToDecimal(output.GPSLongitude[0], output.GPSLongitude[1], output.GPSLongitude[2]);
-    
+
         const thumbnail = await exifr.thumbnail(file);
         this.imagesData.push(output);
         this.imageThumbnails.push(this.getSanitizedUrl(thumbnail));
-    
+
         // Asignar cada imagen a un grupo basado en su proximidad a todas las imágenes en el grupo
         let assigned = false;
         for (let group of this.imageGroups) {
@@ -127,14 +153,27 @@ export class FlightUploadComponent implements OnInit {
         }
         if (!assigned) {
           this.imageGroups.push([output]);
+
+          // Calcula el centro del grupo
+          let latSum = 0, lonSum = 0;
+          for (let image of this.imageGroups[this.imageGroups.length - 1]) {
+            latSum += image.GPSLatitude;
+            lonSum += image.GPSLongitude;
+          }
+          const center: [number, number] = [-lonSum / this.imageGroups[this.imageGroups.length - 1].length, latSum / this.imageGroups[this.imageGroups.length - 1].length] as [number, number];
+
+          // Almacena el centro en cada imagen del grupo
+          for (let image of this.imageGroups[this.imageGroups.length - 1]) {
+            image.center = center;
+          }
         }
       }
-      }
+    }
 
-  
+
     this.addImageMarkersAndPolygon();
   }
-  
+
 
   dmsToDecimal(degrees: number, minutes: number, seconds: number) {
     return degrees + minutes / 60 + seconds / 3600;
@@ -153,6 +192,40 @@ export class FlightUploadComponent implements OnInit {
     const d = R * c; // distancia en km
     return d;
   }
+
+  uploadFiles() {
+    this.uploadStartTime = new Date().getTime();
+
+    // Calcular el tamaño total de los archivos en bytes
+    this.totalBytes = this.selectedFiles.reduce((total, file) => total + file.size, 0);
+
+    this.selectedFiles.forEach(async (file) => {
+      const filePath = `images/${new Date().getTime()}_${file.name}`;
+      this.task = this.storage.upload(filePath, file);
+
+      // Observar el porcentaje de subida
+      this.uploadPercent = this.task.percentageChanges();
+
+      // Observar el estado de la subida y calcular la velocidad
+      this.task.snapshotChanges().subscribe(snapshot => {
+        this.uploadEndTime = new Date().getTime();
+        this.totalBytesTransferred = snapshot.bytesTransferred;
+
+        let elapsedTime = this.uploadEndTime - this.uploadStartTime; // Tiempo transcurrido en milisegundos
+        elapsedTime = elapsedTime / 1000; // Convertir a segundos
+
+        // Calcular la velocidad de subida en MB/segundo
+        this.uploadSpeed = (this.totalBytesTransferred / (1024 * 1024)) / elapsedTime;
+        console.log("Total bytes transfered: " + this.totalBytesTransferred);
+        console.log("Snapshot bytes transfered: " + snapshot.bytesTransferred);
+
+        console.log("Elapsed Time: " + elapsedTime);
+        console.log("Total bytes: " + this.totalBytes);
+
+      });
+    });
+  }
+
 
   deg2rad(deg: number) {
     return deg * (Math.PI / 180);
@@ -190,52 +263,52 @@ export class FlightUploadComponent implements OnInit {
         this.map.removeLayer(layer);
       }
     });
-  
+
     const vectorLayers = [];
-  
+
     this.imageGroups.forEach((group, groupIndex) => {
       const vectorSource = new Vector({
         features: [],
       });
-  
+
       let minLat = Number.POSITIVE_INFINITY;
       let maxLat = Number.NEGATIVE_INFINITY;
       let minLon = Number.POSITIVE_INFINITY;
       let maxLon = Number.NEGATIVE_INFINITY;
-  
+
       group.forEach((image) => {
         const lat = image.GPSLatitude;
         const lon = image.GPSLongitude;
-  
+
         console.log(image.GPSLatitude + " " + image.GPSLongitude)
         minLat = Math.min(minLat, lat);
         maxLat = Math.max(maxLat, lat);
         minLon = Math.min(minLon, lon);
         maxLon = Math.max(maxLon, lon);
-  
+
         const coordinates = transform([-lon, lat], 'EPSG:4326', 'EPSG:3857');
         const feature = new Feature({
           geometry: new Point(coordinates),
         });
-  
+
         const iconStyle = new Style({
           image: new Icon({
-            src: '../../../../../assets/icons/location-pin-dark-unhover.png', 
+            src: '../../../../../assets/icons/location-pin-dark-unhover.png',
             scale: 0.5,
           }),
         });
-  
+
         feature.setStyle(iconStyle);
         vectorSource.addFeature(feature);
       });
-  
+
       const polygonCoordinates = [
         [fromLonLat([-minLon, maxLat]), fromLonLat([-maxLon, maxLat]), fromLonLat([-maxLon, minLat]), fromLonLat([-minLon, minLat]), fromLonLat([-minLon, maxLat])],
       ];
       const polygonFeature = new Feature({
         geometry: new Polygon(polygonCoordinates),
       });
-  
+
       const polygonStyle = new Style({
         stroke: new Stroke({
           color: 'blue',
@@ -245,27 +318,33 @@ export class FlightUploadComponent implements OnInit {
           color: 'rgba(0, 0, 255, 0.1)',
         }),
       });
-  
+
       polygonFeature.setStyle(polygonStyle);
       vectorSource.addFeature(polygonFeature);
-  
+
       const vectorLayer = new VectorLayer({
         source: vectorSource,
       });
-  
+
       vectorLayers.push(vectorLayer);
     });
-  
+
     vectorLayers.forEach(layer => {
       this.map.addLayer(layer);
     });
 
 
-    if(vectorLayers.length > 0 ){
+    if (vectorLayers.length > 0) {
       const lastVectorLayer = vectorLayers[vectorLayers.length - 1];
       const lastPolygonExtent = lastVectorLayer.getSource().getExtent();
       this.map.getView().fit(lastPolygonExtent, { padding: [50, 50, 50, 50], maxZoom: 18 });
     }
 
+  }
+
+  goToGroupCenter(center: [number, number]) {
+    console.log(center);
+    const view = this.map.getView();
+    view.animate({ center: fromLonLat(center), zoom: 16 });
   }
 }
