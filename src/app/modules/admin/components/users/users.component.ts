1a537b2fa6445ee566a7447f238e0a1367a281e9
Ruben <ruben.ferruses@solardrone.es>
2023-08-08 19:37:13 +0200

    Añadido botón para restablecer contraseña del usuario: -En panel admin -En menú de compartir planta (para que el admin de la empresa pueda restablecerla)

--- src/app/modules/admin/components/users/users.component.ts
+++ src/app/modules/admin/components/users/users.component.ts
@@ -7,6 +7,12 @@ import { Subscription } from 'rxjs';
 import { UserService } from '@data/services/user.service';
 
 import { UserInterface } from '@core/models/user';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogConfirmComponent } from '@shared/components/mat-dialog-confirm/mat-dialog-confirm.component';
+import { MatDialog } from '@angular/material/dialog';
+import { take } from 'rxjs/operators';
+import { AuthService } from '@data/services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -17,13 +23,20 @@ export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
   users: UserInterface[];
   displayedColumns: string[] = ['email', 'empresa', 'id', 'actions'];
   dataSource = new MatTableDataSource<any>();
+  statusMessage: string;
 
   private subscriptions = new Subscription();
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild('search') search: ElementRef;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    private authService: AuthService,
+    private http: HttpClient,
+
+  ) { }
 
   ngOnInit(): void {
     this.subscriptions.add(
@@ -52,4 +65,45 @@ export class UsersComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
+
+  confirmResetUserPassword(email: string) {
+    const dialogRef = this.dialog.open(MatDialogConfirmComponent, {
+      data: `Se restablecerá la contraseña del usuario con email "${email}". ¿Desea continuar?`,
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((response: boolean) => {
+        if (response) {
+          this.resetUserPassword(email);
+        }
+      });
+  }
+
+  cloudFunctionUrl = `${this.authService.firebaseFunctionsUrl}/sendEmail`;
+
+  resetUserPassword(email: string) {
+    const payload = {
+      email,
+      template: 'resetPassword',
+    };
+
+    this.http.post(this.cloudFunctionUrl, payload).subscribe(
+      () => {
+        this.statusMessage = 'Contraseña restablecida.';
+        this.openSnackBarMessage("Contraseña del usuario restablecida. El usuario recibirá un email.");
+      },
+      (error) => {
+        this.statusMessage = 'Error al restablecer contraseña.';
+        console.error(error);
+      }
+    );
+  }
+
+  openSnackBarMessage(message: string) {
+    this.snackBar.open(message, 'OK', { duration: 5000 });
+  }
+
+
 }
