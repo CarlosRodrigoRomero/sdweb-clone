8cf4180873ebbd7c9b89558fb79eb63a9841002e
ahuesoa <antolin.h@solardrone.es>
2023-08-17 20:36:54 +0200

    Entorno DEV activado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/* export const environment = {
+export const environment = {
   production: false,
   MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
   firebaseConfig: {
@@ -15,20 +15,20 @@
     appId: '1:372895502043:web:07590a39aaafcf9a7d06fe',
     measurementId: 'G-8993HMQKET',
   },
-}; */
-
-export const environment = {
-  production: false,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
-  firebaseConfig: {
-    apiKey: 'AIzaSyAT1o9xo0dBaLjBFL2HFiG-r_ipKO6_tqc',
-    authDomain: 'sdweb-d33ce.firebaseapp.com',
-    databaseURL: 'https://sdweb-d33ce.firebaseio.com',
-    projectId: 'sdweb-d33ce',
-    storageBucket: 'sdweb-d33ce.appspot.com',
-    messagingSenderId: '229404593483',
-  },
 };
+
+// export const environment = {
+//   production: false,
+//   MAPBOX_API_KEY: 'pk.eyJ1IjoidGFrZXJjaXRvIiwiYSI6ImNqdGJienRqdzBlNjQ0NW56N2ZubnZ5MmwifQ.CCyU9tBn0M1q2ie3Xr9fDg',
+//   firebaseConfig: {
+//     apiKey: 'AIzaSyAT1o9xo0dBaLjBFL2HFiG-r_ipKO6_tqc',
+//     authDomain: 'sdweb-d33ce.firebaseapp.com',
+//     databaseURL: 'https://sdweb-d33ce.firebaseio.com',
+//     projectId: 'sdweb-d33ce',
+//     storageBucket: 'sdweb-d33ce.appspot.com',
+//     messagingSenderId: '229404593483',
+//   },
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
