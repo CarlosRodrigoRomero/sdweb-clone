f7c1703afd164a72ca30b2b08df6eabfa0a77fe5
Ruben <ruben.ferruses@solardrone.es>
2023-08-21 10:13:00 +0200

    Hechos cambios en la forma de clasificar las imágenes: -Se comprueba el número de imágenes térmicas y visuales -Térmicas y visuales se separan en grupos diferentes y se puede ocultar o mostrar cada capa -Se ordenan cronológicamente los grupos -La densidad del vuelo está representada con una linea

--- src/app/modules/admin/components/flight-upload/flight-upload.component.ts
+++ src/app/modules/admin/components/flight-upload/flight-upload.component.ts
@@ -18,7 +18,9 @@ import Polygon from 'ol/geom/Polygon';
 
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
-
+import LineString from 'ol/geom/LineString';
+import geolib from 'geolib';
+import { getDistance } from 'geolib';
 
 export interface ImageData {
   key: string;
@@ -27,12 +29,15 @@ export interface ImageData {
   GPSLongitude: number;
   name: string;
 
-  //Pruebas para clasificar en grupos
+  GPSAltitude: number;
+
   lat?: number;
   long?: number;
   group?: number;
   center?: [number, number]; // Agregar esta línea para almacenar el centro del grupo
 
+  SerialNumber: string;
+  suffix: string;
 }
 
 @Component({
@@ -62,6 +67,11 @@ export class FlightUploadComponent implements OnInit {
   totalBytes: number;
   private subscriptions: Subscription = new Subscription();
 
+  fotoPortadaName: string = '';
+  fotoSuciedadName: string = '';
+
+  public showV: boolean = true;
+  public showT: boolean = true;
 
   ngOnInit(): void {
     this.initMap();
@@ -101,47 +111,56 @@ export class FlightUploadComponent implements OnInit {
   //Pruebas para clasificar en grupos
   currentGroup = 0;
 
+  isDataReady: boolean;
+
+  numeroImagenesTermicas: number = 0;
+  numeroImagenesRGB: number = 0;
+
   constructor(private sanitizer: DomSanitizer,
     private olMapService: OlMapService,
     private storage: AngularFireStorage,
 
   ) { }
+
   async handleChange(event) {
-
     this.selectedFiles = Array.from(event.target.files);
-
-
 
     for (let i = 0; i < event.target.files.length; i++) {
       const file = event.target.files[i];
 
-      // Ignorar los archivos que no sean jpg dando un aviso al usuario
       if (file.type !== 'image/jpeg') {
         alert(`Estás intentando subir el archivo ${file.name} y es de tipo ${file.type}. Este archivo se ignorará. Solo se procesarán archivos .jpg`);
         continue;
       }
 
-      const output: ImageData = await exifr.parse(file, ['GPSLatitude', 'GPSLongitude']);
+      const output: ImageData = await exifr.parse(file);
 
-      // Comprobar si el archivo tiene información de coordenadas GPS
       if (!output || !output.GPSLatitude || !output.GPSLongitude) {
         alert(`El archivo ${file.name} no contiene información de GPS. Este archivo se ignorará.`);
-      }
-      else {
-
+      } else {
         output.name = file.name;
-
-        // Convertir las coordenadas de DMS a decimal
         output.GPSLatitude = this.dmsToDecimal(output.GPSLatitude[0], output.GPSLatitude[1], output.GPSLatitude[2]);
         output.GPSLongitude = this.dmsToDecimal(output.GPSLongitude[0], output.GPSLongitude[1], output.GPSLongitude[2]);
-
         const thumbnail = await exifr.thumbnail(file);
         this.imagesData.push(output);
         this.imageThumbnails.push(this.getSanitizedUrl(thumbnail));
 
-        // Asignar cada imagen a un grupo basado en su proximidad a todas las imágenes en el grupo
         let assigned = false;
+        let currentSuffix = this.getSuffix(output.name);
+        console.log("Current suffix: " + currentSuffix);
+        output.suffix = currentSuffix;
+        if (currentSuffix == "V") {
+          this.numeroImagenesRGB++;
+        } else if (currentSuffix == "T") {
+          this.numeroImagenesTermicas++;
+        }
         for (let group of this.imageGroups) {
+          // Verificar si el número de serie de la imagen es el mismo del grupo
+          if (group[0].SerialNumber !== output.SerialNumber) continue;
+
+          // Luego, verificar si el sufijo de la imagen es el mismo del grupo
+          if (this.getSuffix(group[0].name) !== currentSuffix) continue;
+
           for (let groupImage of group) {
             if (this.calculateDistance(output.GPSLatitude, output.GPSLongitude, groupImage.GPSLatitude, groupImage.GPSLongitude) <= 0.5) {
               group.push(output);
@@ -151,18 +170,15 @@ export class FlightUploadComponent implements OnInit {
           }
           if (assigned) break;
         }
+
         if (!assigned) {
           this.imageGroups.push([output]);
-
-          // Calcula el centro del grupo
           let latSum = 0, lonSum = 0;
           for (let image of this.imageGroups[this.imageGroups.length - 1]) {
             latSum += image.GPSLatitude;
             lonSum += image.GPSLongitude;
           }
           const center: [number, number] = [-lonSum / this.imageGroups[this.imageGroups.length - 1].length, latSum / this.imageGroups[this.imageGroups.length - 1].length] as [number, number];
-
-          // Almacena el centro en cada imagen del grupo
           for (let image of this.imageGroups[this.imageGroups.length - 1]) {
             image.center = center;
           }
@@ -170,9 +186,47 @@ export class FlightUploadComponent implements OnInit {
       }
     }
 
+    console.log(this.imageGroups);
 
+
+    this.imageGroups.forEach(group => {
+      group.sort((a, b) => {
+        const dateA = new Date(this.extractDateTimeFromName(a.name)).getTime();
+        const dateB = new Date(this.extractDateTimeFromName(b.name)).getTime();
+
+        if (dateA === dateB) {
+          const numA = this.extractNumberFromName(a.name);
+          const numB = this.extractNumberFromName(b.name);
+          return numA - numB;
+        }
+
+        return dateA - dateB;
+      });
+    });
+
+
+
+    console.log(this.imageGroups);
     this.addImageMarkersAndPolygon();
+
+    for (let i = 0; i < this.imageGroups.length; i++) {
+      this.verificarAltitud(this.imageGroups[i]);
+      let distanciaModaGrupo = this.distanciaModa(this.imageGroups[i]);
+      this.drawRouteLines(this.imageGroups[i], distanciaModaGrupo, i);
+    }
+
+    if(this.numeroImagenesRGB != this.numeroImagenesTermicas){
+      alert(`El número de imágenes RGB y térmicas NO COINCIDE. Imágenes RGB: ${this.numeroImagenesRGB}, Imágenes térmicas ${this.numeroImagenesTermicas}.`)
+    }else{
+      alert(`El número de imágenes RGB y térmicas SÍ COINCIDE. Imágenes RGB: ${this.numeroImagenesRGB}, Imágenes térmicas ${this.numeroImagenesTermicas}.`)
+
+    }
+
+    this.isDataReady = true;
+
   }
+
+
 
 
   dmsToDecimal(degrees: number, minutes: number, seconds: number) {
@@ -271,6 +325,8 @@ export class FlightUploadComponent implements OnInit {
         features: [],
       });
 
+
+
       let minLat = Number.POSITIVE_INFINITY;
       let maxLat = Number.NEGATIVE_INFINITY;
       let minLon = Number.POSITIVE_INFINITY;
@@ -291,12 +347,27 @@ export class FlightUploadComponent implements OnInit {
           geometry: new Point(coordinates),
         });
 
-        const iconStyle = new Style({
-          image: new Icon({
-            src: '../../../../../assets/icons/location-pin-dark-unhover.png',
-            scale: 0.5,
-          }),
-        });
+        let iconStyle;
+        if (group[0].suffix == "V") {
+          iconStyle = new Style({
+            image: new Icon({
+              src: '../../../../../assets/icons/location-pin-dark-unhover.png',
+              scale: 0.35,
+            }),
+          });
+        }
+
+        else if (group[0].suffix == "T") {
+          iconStyle = new Style({
+            image: new Icon({
+              src: '../../../../../assets/icons/location-pin-light-hover.png',
+              scale: 0.35,
+            }),
+          });
+        }
+
+
+
 
         feature.setStyle(iconStyle);
         vectorSource.addFeature(feature);
@@ -309,22 +380,43 @@ export class FlightUploadComponent implements OnInit {
         geometry: new Polygon(polygonCoordinates),
       });
 
-      const polygonStyle = new Style({
-        stroke: new Stroke({
-          color: 'blue',
-          width: 2,
-        }),
-        fill: new Fill({
-          color: 'rgba(0, 0, 255, 0.1)',
-        }),
-      });
 
+      let polygonStyle;
+
+      if (group[0].suffix == "V") {
+
+        polygonStyle = new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            width: 2,
+          }),
+          fill: new Fill({
+            color: 'rgba(0, 0, 255, 0.1)',
+          }),
+        });
+
+      }
+      else if (group[0].suffix == "T") {
+        polygonStyle = new Style({
+          stroke: new Stroke({
+            color: 'red',
+            width: 2,
+          }),
+          fill: new Fill({
+            color: 'rgba(255, 0, 0, 0.1)',
+          }),
+        });
+      }
       polygonFeature.setStyle(polygonStyle);
       vectorSource.addFeature(polygonFeature);
 
       const vectorLayer = new VectorLayer({
         source: vectorSource,
       });
+
+      // Asignar el suffix a la capa aquí
+      vectorLayer.set('suffix', group[0].suffix);
+
 
       vectorLayers.push(vectorLayer);
     });
@@ -347,4 +439,180 @@ export class FlightUploadComponent implements OnInit {
     const view = this.map.getView();
     view.animate({ center: fromLonLat(center), zoom: 16 });
   }
+
+
+  distanciaModa(datos) {
+    const distancias = [];
+    for (let i = 1; i < datos.length; i++) {
+      const distancia = getDistance(
+        { latitude: datos[i - 1].latitude, longitude: datos[i - 1].longitude },
+        { latitude: datos[i].latitude, longitude: datos[i].longitude }
+      );
+      distancias.push(Math.round(distancia * 10) / 10);
+    }
+
+    const counts = {};
+    distancias.forEach((distancia) => {
+      counts[distancia] = (counts[distancia] || 0) + 1;
+    });
+
+    let maxCount = 0;
+    let moda = null;
+    for (const distancia in counts) {
+      if (counts[distancia] > maxCount) {
+        maxCount = counts[distancia];
+        moda = distancia;
+      }
+    }
+    console.log("Moda: " + moda);
+    return moda;
+  }
+
+
+  drawRouteLines(group: ImageData[], distanciaModa: number, groupIndex) {
+    // console.log("entered on drawRouteLines");
+    const topeModa = distanciaModa * 2;
+
+    for (let i = 1; i < group.length; i++) {
+      const coordinates = []; // Inicializar un nuevo arreglo para cada línea
+      const coord1 = [-group[i - 1].GPSLongitude, group[i - 1].GPSLatitude];
+      const coord2 = [-group[i].GPSLongitude, group[i].GPSLatitude];
+      const distancia = this.calculateDistance(coord1[1], coord1[0], coord2[1], coord2[0]) * 1000;
+
+      let color;
+
+      if (distancia <= topeModa) {
+        color = 'green';
+      } else if (topeModa < distancia && distancia <= distanciaModa * 3) {
+        const factorTransicion = (distancia - topeModa) / (distanciaModa);
+        const colorVerde = Math.floor((1 - factorTransicion) * 255);
+        const colorRojo = Math.floor(factorTransicion * 255);
+        color = `rgb(${colorRojo}, ${colorVerde}, 0)`;
+      } else {
+        color = 'red';
+      }
+
+      coordinates.push(fromLonLat(coord1));
+      coordinates.push(fromLonLat(coord2));
+
+      const lineString = new LineString(coordinates);
+      const feature = new Feature({ geometry: lineString });
+      const lineStyle = new Style({
+        stroke: new Stroke({
+          color: color,
+          width: 6,
+        }),
+      });
+      feature.setStyle(lineStyle);
+
+      // console.table(this.map.getLayers());
+      // Assuming vectorSource is the Vector source for the group
+      const layer = this.map.getLayers().item(groupIndex + 1);
+      // console.log('layer:', layer);
+
+      if (layer instanceof VectorLayer || layer instanceof TileLayer) {
+        const vectorSource = layer.getSource();
+        // console.log("Vector source: " + vectorSource);
+        // console.table(vectorSource);
+        vectorSource.addFeature(feature);
+
+        // console.log("Added feature to vectorSource: ")
+        // console.table(feature);
+      }
+    }
+  }
+
+  handleFotoPortadaChange(event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      this.fotoPortadaName = input.files[0].name;
+    }
+  }
+  handleFotoSuciedadChange(event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      this.fotoSuciedadName = input.files[0].name;
+    }
+  }
+
+
+  extractDateTimeFromName(name) {
+    const match = name.match(/^DJI_(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})_/);
+    if (match) {
+      const year = parseInt(match[1], 10);
+      const month = parseInt(match[2], 10) - 1;  // Meses en JavaScript son 0-indexados
+      const day = parseInt(match[3], 10);
+      const hour = parseInt(match[4], 10);
+      const minute = parseInt(match[5], 10);
+      const second = parseInt(match[6], 10);
+      return new Date(year, month, day, hour, minute, second);
+    }
+    return null;
+  }
+
+
+  getSuffix(name) {
+    // La expresión regular busca un guion bajo seguido de una 'V' o 'T', justo antes de la extensión del archivo (e.g. .jpg)
+    const regex = /_([VT])\.\w+$/;
+    const match = name.match(regex);
+    return match ? match[1] : null;
+  }
+
+  toggleLayers(suffix: string, isVisible: boolean) {
+    this.map.getLayers().forEach(layer => {
+      if (layer instanceof VectorLayer && layer.get("suffix") === suffix) {
+        layer.setVisible(isVisible);
+      }
+    });
+  }
+
+  extractNumberFromName(name: string): number {
+    const match = name.match(/_(\d+)_/);
+    if (match) {
+      return parseInt(match[1], 10);
+    }
+    return -1;  // Retorna un valor por defecto en caso de no encontrar el número.
+  }
+
+  calcularModa(valores) {
+    const counts = {};
+    valores.forEach((valor) => {
+      counts[valor] = (counts[valor] || 0) + 1;
+    });
+
+    let maxCount = 0;
+    let moda = null;
+    for (const valor in counts) {
+      if (counts[valor] > maxCount) {
+        maxCount = counts[valor];
+        moda = parseFloat(valor);
+      }
+    }
+    return moda;
+  }
+
+
+  verificarAltitud(datos) {
+    const altitudes = datos.map(dato => {
+      if (dato.GPSAltitude !== null && dato.GPSAltitude !== undefined) {
+        return parseFloat(dato.GPSAltitude.toFixed(1));
+      } else {
+        console.error(`El dato con el nombre ${dato.name} no tiene un valor de GPSAltitude definido.`);
+        return null;  // Puedes devolver null o algún valor predeterminado.
+      }
+    }).filter(altitud => altitud !== null);  // Filtra cualquier altitud que sea null.
+
+    const modaAltitud = this.calcularModa(altitudes);
+
+    datos.forEach((dato) => {
+      if (dato.GPSAltitude && Math.abs(dato.GPSAltitude - modaAltitud) > 10) {
+        alert(`¡Atención! La imagen con nombre ${dato.name} tiene una altitud que varía en más de 10 metros respecto a la moda.`);
+      }
+    });
+  }
+
+
 }
+
