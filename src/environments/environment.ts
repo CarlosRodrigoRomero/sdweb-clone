11974da0e83e9211a6db8ee2c756007db2ce0dc4
ahuesoa <antolin.h@solardrone.es>
2023-08-25 16:45:44 +0200

    Dev: desactivado proyecto dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,31 +2,31 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBDnFom38AIb6OL7MFP53gaT9RoHl_CAx8',
-    authDomain: 'sdweb-dev.firebaseapp.com',
-    databaseURL: 'https://sdweb-dev.firebaseio.com',
-    projectId: 'sdweb-dev',
-    storageBucket: 'sdweb-dev.appspot.com',
-    messagingSenderId: '372895502043',
-    appId: '1:372895502043:web:07590a39aaafcf9a7d06fe',
-    measurementId: 'G-8993HMQKET',
-  },
-};
-
 // export const environment = {
 //   production: false,
 //   firebaseConfig: {
-//     apiKey: 'AIzaSyAT1o9xo0dBaLjBFL2HFiG-r_ipKO6_tqc',
-//     authDomain: 'sdweb-d33ce.firebaseapp.com',
-//     databaseURL: 'https://sdweb-d33ce.firebaseio.com',
-//     projectId: 'sdweb-d33ce',
-//     storageBucket: 'sdweb-d33ce.appspot.com',
-//     messagingSenderId: '229404593483',
+//     apiKey: 'AIzaSyBDnFom38AIb6OL7MFP53gaT9RoHl_CAx8',
+//     authDomain: 'sdweb-dev.firebaseapp.com',
+//     databaseURL: 'https://sdweb-dev.firebaseio.com',
+//     projectId: 'sdweb-dev',
+//     storageBucket: 'sdweb-dev.appspot.com',
+//     messagingSenderId: '372895502043',
+//     appId: '1:372895502043:web:07590a39aaafcf9a7d06fe',
+//     measurementId: 'G-8993HMQKET',
 //   },
 // };
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAT1o9xo0dBaLjBFL2HFiG-r_ipKO6_tqc',
+    authDomain: 'sdweb-d33ce.firebaseapp.com',
+    databaseURL: 'https://sdweb-d33ce.firebaseio.com',
+    projectId: 'sdweb-d33ce',
+    storageBucket: 'sdweb-d33ce.appspot.com',
+    messagingSenderId: '229404593483',
+  },
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
