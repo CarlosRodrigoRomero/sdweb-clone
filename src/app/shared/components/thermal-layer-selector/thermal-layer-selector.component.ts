72e2d9d49d76eccd134c5750fd17034f2db03f85
ahuesoa <antolin.h@solardrone.es>
2023-08-28 18:48:16 +0200

    Slider termico modificando cada informe concreto

--- src/app/shared/components/thermal-layer-selector/thermal-layer-selector.component.ts
+++ src/app/shared/components/thermal-layer-selector/thermal-layer-selector.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { ReportControlService } from '@data/services/report-control.service';
 import { ViewReportService } from '@data/services/view-report.service';
 
 @Component({
@@ -7,10 +10,24 @@ import { ViewReportService } from '@data/services/view-report.service';
   templateUrl: './thermal-layer-selector.component.html',
   styleUrls: ['./thermal-layer-selector.component.css'],
 })
-export class ThermalLayerSelectorComponent {
-  constructor(private viewReportService: ViewReportService) {}
+export class ThermalLayerSelectorComponent implements OnInit, OnDestroy {
+  selectedInformeId: string;
+
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(private viewReportService: ViewReportService, private reportControlService: ReportControlService) {}
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.reportControlService.selectedInformeId$.subscribe((informeId) => (this.selectedInformeId = informeId))
+    );
+  }
 
   setThermalLayerStatus(event: any) {
     this.viewReportService.thermalLayerVisible = event.checked;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
