493b4ba70ae1ece8b3fe0e870b825e6195139421
ahuesoa <antolin.h@solardrone.es>
2023-08-28 19:40:32 +0200

    Elementos inferiores mapa metidos en menu capas

--- src/app/modules/shared-plants/components/layers-panel/layers-panel.component.ts
+++ src/app/modules/shared-plants/components/layers-panel/layers-panel.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { ZonesService } from '@data/services/zones.service';
 
 @Component({
   selector: 'app-layers-panel',
   templateUrl: './layers-panel.component.html',
-  styleUrls: ['./layers-panel.component.css']
+  styleUrls: ['./layers-panel.component.css'],
 })
-export class LayersPanelComponent implements OnInit {
+export class LayersPanelComponent implements OnInit, OnDestroy {
+  thereAreLargestZones = false;
 
-  constructor() { }
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(private zonesService: ZonesService) {}
 
   ngOnInit(): void {
+    this.subscriptions.add(
+      this.zonesService.thereAreLargestZones$.subscribe((value) => (this.thereAreLargestZones = value))
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
