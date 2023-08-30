ae0d20aac7d7d36f978cd0ba0666088e35e5a06b
ahuesoa <antolin.h@solardrone.es>
2023-08-30 21:21:03 +0200

    S2E no mostrando activar capa termica

--- src/app/modules/shared-plants/components/layers-panel/layers-panel.component.ts
+++ src/app/modules/shared-plants/components/layers-panel/layers-panel.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { ZonesService } from '@data/services/zones.service';
+import { ReportControlService } from '@data/services/report-control.service';
 
 @Component({
   selector: 'app-layers-panel',
@@ -11,15 +12,18 @@ import { ZonesService } from '@data/services/zones.service';
 })
 export class LayersPanelComponent implements OnInit, OnDestroy {
   thereAreLargestZones = false;
+  showThermalLayer = true;
 
   private subscriptions: Subscription = new Subscription();
 
-  constructor(private zonesService: ZonesService) {}
+  constructor(private zonesService: ZonesService, private reportControlService: ReportControlService) {}
 
   ngOnInit(): void {
     this.subscriptions.add(
       this.zonesService.thereAreLargestZones$.subscribe((value) => (this.thereAreLargestZones = value))
     );
+
+    this.showThermalLayer = this.reportControlService.plantaFija;
   }
 
   ngOnDestroy(): void {
